e979f202940c84a6569c5ce4c93192b4953ce178
Joshua Briones <joshuabriones2003@gmail.com>
2024-04-04 21:46:49 +0800

    Initial commit

---
+++ app/login/page.jsx
@@ -0,0 +1,55 @@
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { supabase } from "@/utils/supabase/supabaseClient";
+
+const Login = () => {
+  const router = useRouter();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const { user, error } = await supabase.auth.signIn({
+        email,
+        password,
+      });
+
+      if (error) {
+        console.error("Login error:", error.message);
+      } else {
+        console.log("Login successful:", user);
+        router.push("/"); // Redirect to home or landing page
+      }
+    } catch (error) {
+      console.error("Login error:", error.message);
+    }
+  };
+
+  return (
+    <form onSubmit={handleLogin}>
+      <h2>Login</h2>
+      <label>Email:</label>
+      <input
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        required
+      />
+      <label>Password:</label>
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        required
+      />
+      <button type="submit" className="p-5 bg-red-600">
+        Login
+      </button>
+    </form>
+  );
+};
+
+export default Login;
---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+import { supabase } from "@/utils/supabase/supabaseClient";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen max-w-screen-2xl bg-yellow-100">
+      <section>THIS IS THE HOME PAGE</section>
+    </main>
+  );
+}
