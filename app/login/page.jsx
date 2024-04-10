ffbf7ce18058a209b60b49e81fbf13b9a2d25f42
Gieb Mier <128208941+giebm@users.noreply.github.com>
2024-04-10 23:42:52 +0800

    adjusted login page, frontend

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -12,127 +12,137 @@ import HollowButton from "@/components/ui/HollowButton";
 import TextInput from "@/components/ui/TextInput";
 
 const Login = () => {
-  const router = useRouter();
+	const router = useRouter();
 
-  const [showInvalidCredentials, setShowInvalidCredentials] = useState(false);
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+	const [showInvalidCredentials, setShowInvalidCredentials] = useState(false);
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
 
-  const handleLogin = async (e) => {
-    e.preventDefault();
+	const handleLogin = async (e) => {
+		e.preventDefault();
 
-    const formData = new FormData();
-    formData.append("email", email);
-    formData.append("password", password);
+		const formData = new FormData();
+		formData.append("email", email);
+		formData.append("password", password);
 
-    try {
-      const response = await fetch("/api/auth/login", {
-        method: "POST",
-        body: formData,
-      });
+		try {
+			const response = await fetch("/api/auth/login", {
+				method: "POST",
+				body: formData,
+			});
 
-      if (response.ok) {
-        console.log("Login successful:");
-        router.push("/");
-      } else {
-        console.error("Login error: inside", error.message);
-      }
-    } catch (error) {
-      console.error("Login error:", error.message);
-    }
+			if (response.ok) {
+				console.log("Login successful:");
+				router.push("/");
+			} else {
+				console.error("Login error: inside", error.message);
+			}
+		} catch (error) {
+			console.error("Login error:", error.message);
+		}
 
-    setShowInvalidCredentials(true);
-    // Hide the message after 5 seconds
-    setTimeout(() => {
-      setShowInvalidCredentials(false);
-    }, 5000);
+		setShowInvalidCredentials(true);
+		// Hide the message after 5 seconds
+		setTimeout(() => {
+			setShowInvalidCredentials(false);
+		}, 5000);
 
-    alert("Email: " + email + " Password: " + password);
-  };
+		alert("Email: " + email + " Password: " + password);
+	};
 
-  const handleCreateAccount = async (e) => {
-    e.preventDefault();
-    alert("Create Account");
-  };
+	const handleCreateAccount = async (e) => {
+		e.preventDefault();
+		alert("Create Account");
+	};
 
-  return (
-    <div
-      className="min-h-screen w-full"
-      style={{
-        backgroundImage: `url(${loginBg.src})`,
-        width: "100%",
-        height: "100%",
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-      }}
-    >
-      {/* navigation bar */}
-      <div className="h-20 w-full bg-white flex flex-row justify-between items-center px-44">
-        <div className="text-2xl text-[#6B9080] font-bold">WellTalk</div>
-        <div className="flex flex-row gap-x-16">
-          <div className="text-sm">Home</div>
-          <div className="text-sm">About</div>
-          <div className="text-sm">Contact</div>
-        </div>
-      </div>
+	return (
+		<div
+			className="min-h-screen w-full"
+			style={{
+				backgroundImage: `url(${loginBg.src})`,
+				width: "100%",
+				height: "100%",
+				backgroundSize: "cover",
+				backgroundAttachment: "fixed",
+				backgroundPosition: "center",
+			}}
+		>
+			{/* navigation bar */}
+			<div className="h-20 w-full bg-white flex flex-row justify-between items-center px-44">
+				<div className="text-2xl text-[#6B9080] font-Merriweather font-bold">
+					WellTalk
+				</div>
+				<div className="flex flex-row gap-x-16">
+					<div className="text-lg font-Jaldi">Home</div>
+					<div className="text-lg font-Jaldi">About</div>
+					<div className="text-lg font-Jaldi">Contact</div>
+				</div>
+			</div>
 
-      {/* login form*/}
-      <div className="flex justify-start items-center py-5 px-48 ">
-        <form
-          className="w-5/12 h-[650px] flex flex-col justify-center"
-          onSubmit={handleLogin}
-        >
-          <p className="text-black text-5xl pb-3">Sign in</p>
-				{/* login form*/}
-				<div className="flex justify-start items-center py-5 px-48 ">
-					<form
-						className="w-5/12 h-[700px] flex flex-col justify-center"
-						onSubmit={handleLogin}>
-						<p className="text-black text-5xl pb-3">Sign in</p>
+			{/* login form*/}
+			<div className="flex justify-start items-center py-5 px-48 ">
+				<div className="w-7/12 h-[650px] flex flex-col justify-center">
+					{/* login form*/}
+					<div className="flex justify-start items-center py-5 px-48 ">
+						<form
+							className="w-full h-[700px] flex flex-col justify-center"
+							onSubmit={handleLogin}
+						>
+							<p className="text-black text-5xl font-Merriweather py-20 pb-3.5">
+								Sign in
+							</p>
 
-	          {/* error message */}
-	          {showInvalidCredentials && (
-	            <div className="text-red-500 font-bold text-base pt-2 pb-1.5">
-	              Invalid email or password. Try Again.
-	            </div>
-	          )}
+							{/* error message */}
+							{showInvalidCredentials && (
+								<div className="text-red-500 font-bold text-base pt-2 pb-1.5">
+									Invalid email or password. Try Again.
+								</div>
+							)}
 
-	          {/* form inputs */}
-	          <div className="flex flex-col gap-y-3 pb-14">
-	            <TextInput
-	              label="Email Address"
-	              value={email}
-	              onChange={(e) => setEmail(e.target.value)}
-	              type="email"
-	            />
-	            <TextInput
-	              label="Password"
-	              value={password}
-	              onChange={(e) => setPassword(e.target.value)}
-	              type="password"
-	            />
-	          </div>
+							{/* form inputs */}
+							<div className="flex flex-col gap-y-3 pb-14">
+								<TextInput
+									label="Email Address"
+									value={email}
+									onChange={(e) => setEmail(e.target.value)}
+									type="email"
+								/>
+								<TextInput
+									label="Password"
+									value={password}
+									onChange={(e) =>
+										setPassword(e.target.value)
+									}
+									type="password"
+								/>
+							</div>
 
-          {/*to be made into component */}
-          <div className="w-full flex flex-row gap-x-8 pb-12">
-            <HollowButton className="w-1/2" onClick={handleLogin}>
-              Sign In
-            </HollowButton>
-            <FullButton className="w-1/2" onClick={handleCreateAccount}>
-              Create Account
-            </FullButton>
-          </div>
+							{/*to be made into component */}
+							<div className="w-full flex flex-row gap-x-8 pb-12">
+								<HollowButton
+									className="w-1/2"
+									onClick={handleLogin}
+								>
+									Sign In
+								</HollowButton>
+								<FullButton
+									className="w-1/2"
+									onClick={handleCreateAccount}
+								>
+									Create Account
+								</FullButton>
+							</div>
 
-          {/* forgot password */}
-          <div className="text-[#6B9080] text-sm pt-4 hover:text-green-800">
-            Forgot Password?
-          </div>
-        </form>
-      </div>
-	  </form>
-    </div>
-	</div>
-  );
+							{/* forgot password */}
+							<div className="text-[#6B9080] text-lg font-Jaldi pt-4 hover:text-green-800">
+								Forgot Password?
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Login;
