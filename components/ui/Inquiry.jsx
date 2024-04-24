acc68beca5be0dc4defd21ee0a4d32ca1eff64b1
showah <146901959+showah@users.noreply.github.com>
2024-04-24 18:20:10 +0800

    added student inquiry frontend

--- components/ui/Inquiry.jsx
+++ components/ui/Inquiry.jsx
@@ -1,50 +1,50 @@
-import React from 'react';
+import { useState } from "react";
+import SubjectInput from "./inputs/InputSubject";
+import QuestionInput from "./inputs/InputQuestion";
 
 const Inquiry = () => {
-  return (
-    <section id="cta" className="bg-gray-50">
-      <div className="container mx-auto xl:px-60 py-10 px-5 lg:py-40">
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
-          <div className="lg:col-span-2">
-            <h2 className="font-Merriweather text-3xl lg:text-6xl">
-              Need to ask <br /> Something?
-            </h2>
-            <p className="pt-5 font-Jaldi text-base lg:text-lg">
-              Have a Question or Concern? Our Supportive Team is Here to
-              Listen, <br />
-              Offer Guidance, and Provide the Help You Need. Don’t Hesitate To
-              Ask.
-            </p>
-          </div>
-          <div className="mt-6 lg:mt-0">
-            <div className="grid grid-cols-1 gap-2">
-              <label className="block">
-                <input
-                  type="text"
-                  className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-gray-500 focus:ring-gray-300 focus:ring-opacity-50"
-                  placeholder="Your Name"
-                />
-              </label>
-              <label className="block">
-                <textarea
-                  rows={5}
-                  className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-gray-500 focus:ring-gray-300 focus:ring-opacity-50"
-                  placeholder="Your Message"
-                ></textarea>
-              </label>
-              <button
-                className="rounded-md text-white uppercase p-3 w-full"
-                type="button"
-                style={{ backgroundColor: '#6B9080' }}
-              >
-                Subscribe Now
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+	const [subject, setSubject] = useState("");
+	const [question, setQuestion] = useState("");
+	return (
+		<section
+			id="cta"
+			className="bg-gray-50">
+			<div className="container mx-auto xl:px-60 py-10 px-5 lg:py-40">
+				<div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
+					<div className="lg:col-span-2">
+						<h2 className="font-Merriweather text-3xl lg:text-6xl text-center lg:text-left">
+							Need to ask <br /> Something?
+						</h2>
+						<p className="pt-5 font-Jaldi text-base lg:text-lg text-center lg:text-left">
+							Have a Question or Concern? Our Supportive Team is
+							Here to Listen, <br />
+							Offer Guidance, and Provide the Help You Need. Don’t
+							Hesitate To Ask.
+						</p>
+					</div>
+					<div className="mt-6 lg:mt-0">
+						<div className="grid grid-cols-1 gap-2">
+							<SubjectInput
+								placeholder="Subject"
+								subject={subject}
+								setSubject={setSubject}
+							/>
+							<QuestionInput
+              question={question}
+              setQuestion={setQuestion}
+              />
+							<button
+								className="rounded-md text-white uppercase p-3 w-full"
+								type="button"
+								style={{ backgroundColor: "#6B9080" }}>
+								Submit Inquiry
+							</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default Inquiry;
