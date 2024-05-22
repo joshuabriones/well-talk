027d8ebbccd248463912698d8b4dd8dc2fba9a9a
keisamae <kheisa.selma@cit.edu>
2024-05-22 18:40:36 +0800

    appointment-update

--- app/counselor/counselor-appointment/page.jsx
+++ app/counselor/counselor-appointment/page.jsx
@@ -1,8 +1,8 @@
 "use client";
 
 import hdrAppointment from "@/public/images/headers/hdrAppointment.png";
-import { useState } from "react";
-np;
+import { useState, useEffect } from "react";
+
 // css
 import "@/styles/counselor.css";
 
@@ -12,284 +12,268 @@ import ModalAppointmentInfo from "@/components/ui/modals/counselor/appointments/
 import ModalDelete from "@/components/ui/modals/counselor/inquiries/ModalDelete";
 
 export default function Appointment() {
-	const AppointmentPerPage = 10;
+  const AppointmentPerPage = 10;
 
-	const [selectedID, setSelectedID] = useState(null);
-	const [currentPage, setCurrentPage] = useState(1);
+  const [selectedID, setSelectedID] = useState(null);
+  const [currentPage, setCurrentPage] = useState(1);
 
-	//modals
-	const [deleteModal, setDeleteModal] = useState(false);
-	const [appointmentModal, setAppointmentModal] = useState(null);
+  //modals
+  const [deleteModal, setDeleteModal] = useState(false);
+  const [appointmentModal, setAppointmentModal] = useState(null);
 
-	// appointment sample
-	const [appointments, setAppointments] = useState([
-		{
-			id: 20,
-			dateTime: "Sep 8, 2024 3:30 PM",
-			date: "Sep 8, 2024",
-			time: "9:00 AM to 10:00 AM",
-			setter: {
-				name: "Charlotte Johnson",
-				email: "charlotte.johnson@example.com",
-				avatar: "/avatar-charlotte.png",
-				idNumber: "21-2345-678",
-			},
-			subject: "Request for extended warranty",
-			details:
-				"Nam fringilla sapien sed libero finibus ultrices. Proin sed libero vestibulum, maximus nulla nec, fermentum odio.",
-			status: "Responded", // Pending, Responded, Appointed
-		},
-		{
-			id: 21,
-			dateTime: "Sep 8, 2024 3:30 PM",
-			setter: {
-				name: "Charlotte Johnson",
-				email: "charlotte.johnson@example.com",
-				avatar: "/avatar-charlotte.png",
-				idNumber: "21-2345-678",
-			},
-			subject: "Request for extended warranty",
-			details:
-				"Nam fringilla sapien sed libero finibus ultrices. Proin sed libero vestibulum, maximus nulla nec, fermentum odio.",
-			status: "Pending", // Pending, Responded, Appointed
-		},
-		{
-			id: 22,
-			dateTime: "Sep 8, 2024 3:30 PM",
-			setter: {
-				name: "Charlotte Johnson",
-				email: "charlotte.johnson@example.com",
-				avatar: "/avatar-charlotte.png",
-				idNumber: "21-2345-678",
-			},
-			subject: "Request for extended warranty",
-			details:
-				"Nam fringilla sapien sed libero finibus ultrices. Proin sed libero vestibulum, maximus nulla nec, fermentum odio.",
-			status: "Appointed", // Pending, Responded, Appointed
-		},
-	]);
+  const [appointments, setAppointments] = useState([]);
 
-	const handleRowClick = (id) => {
-		setSelectedID(id);
-		setAppointmentModal(true);
-	};
+  useEffect(() => {
+    try {
+      const fetchAppointments = async () => {
+        const response = await fetch("/api/appointment/view-appointments");
+        const data = await response.json();
+        setAppointments(data.appointments);
+      };
 
-	const showDeleteModal = (id) => {
-		setSelectedID(id);
-		setDeleteModal(true);
-	};
+      fetchAppointments();
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
 
-	const handleDelete = () => {
-		// Find
-		const selected = appointments.find((appointment) => appointment.id === selectedID);
+  const formatDate = (date) => {
+    const dateObject = new Date(date);
+    const options = { year: "numeric", month: "long", day: "numeric" };
+    const finalDate = dateObject.toLocaleDateString("en-US", options);
 
-		// Delete
-		const newAppointments = appointments.filter((appointment) => appointment.id !== selectedID);
-		setAppointments(newAppointments);
+    return finalDate;
+  };
 
-		// Reset
-		setDeleteModal(false);
-		setSelectedID(null);
-	};
+  const handleRowClick = (id) => {
+    setSelectedID(id);
+    setAppointmentModal(true);
+  };
 
-	// handle reschedule // TO BE ADDED AFTER CALENDAR IMPLEMENTATION
-	// const handleReschedule = () => {
-	// 	// Find
-	// 	const selected = appointments.find(
-	// 		(appointment) => appointment.id === selectedID
-	// 	);
+  const showDeleteModal = (id) => {
+    setSelectedID(id);
+    setDeleteModal(true);
+  };
 
-	// Calculate the index range of appointment to display for the current page
-	const indexOfLastInquiry = currentPage * AppointmentPerPage;
-	const indexOfFirstInquiry = indexOfLastInquiry - AppointmentPerPage;
-	const currentAppointments = appointments?.slice(indexOfFirstInquiry, indexOfLastInquiry);
+  const handleDelete = () => {
+    // Find
+    const selected = appointments.find(
+      (appointment) => appointment.id === selectedID
+    );
 
-	return (
-		<div className="min-h-screen w-full">
-			{/* navigation bar */}
-			<Navbar userType="counselor" />
+    // Delete
+    const newAppointments = appointments.filter(
+      (appointment) => appointment.id !== selectedID
+    );
+    setAppointments(newAppointments);
 
-			{/* header */}
-			<div className="w-full h-[55vh] relative">
-				{/* Background image */}
-				<div
-					className="absolute inset-0 bg-cover bg-center opacity-40"
-					style={{
-						backgroundImage: `url(${hdrAppointment.src})`,
-					}}
-				></div>
+    // Reset
+    setDeleteModal(false);
+    setSelectedID(null);
+  };
 
-				{/* Content */}
-				<div className="relative z-10 flex items-center justify-center h-full">
-					<div className="flex flex-col text-left px-44 py-10 gap-y-4">
-						<h1 className="font-Merriweather text-8xl">Appointments</h1>
-						<p className="w-1/2 font-Jaldi text-xl">
-							Manage sessions effortlessly and provide tailored guidance and support
-							to students through efficient booking and coordination. Streamline your
-							scheduling process and ensure students receive personalized attention.
-						</p>
-					</div>
-				</div>
-			</div>
+  // handle reschedule // TO BE ADDED AFTER CALENDAR IMPLEMENTATION
+  // const handleReschedule = () => {
+  // 	// Find
+  // 	const selected = appointments.find(
+  // 		(appointment) => appointment.id === selectedID
+  // 	);
 
-			<div className="flex flex-col text-center">
-				{/* table*/}
-				<div className="overflow-x-auto px-56 py-10 ">
-					<table className="table bg-gray-100">
-						{/* head */}
-						<thead>
-							<tr className="bg-gray-200 font-bold">
-								<th className="text-center p-5">ID</th>
-								<th>Date and Time</th>
-								<th className="p-5">ID Number</th>
-								<th>Student</th>
-								<th className="">Reason</th>
-								<th className="text-center">Status</th>
-								{/* Delete and Edit*/}
-								<th className="no-hover-highlight"></th>
-							</tr>
-						</thead>
-						<tbody>
-							{currentAppointments?.map((appointments) => (
-								<tr
-									key={appointments.id}
-									onClick={() => handleRowClick(appointments.id)}
-									className="cursor-pointer hover:bg-gray-200 transition duration-300 ease-in-out"
-								>
-									<td className="text-center">{appointments.id}</td>
-									<td>
-										<div className="flex flex-row gap-x-3">
-											<div className="text-sm">{appointments.dateTime}</div>
-										</div>
-									</td>
-									<td>
-										<div className="flex flex-row gap-x-3">
-											<div>{appointments.setter.idNumber}</div>
-										</div>
-									</td>
-									<td>
-										<div className="flex items-center gap-3">
-											<div className="avatar">
-												<div className="mask mask-squircle w-12 h-12">
-													<img
-														src={appointments.setter.avatar}
-														alt="Avatar Tailwind CSS Component"
-													/>
-												</div>
-											</div>
-											<div>
-												<div className="font-bold">
-													{appointments.setter.name}
-												</div>
-												<div className="text-sm opacity-50">
-													{appointments.setter.email}
-												</div>
-											</div>
-										</div>
-									</td>
-									<td>
-										<p>
-											{appointments.subject.length > 50
-												? `${appointments.subject.substring(0, 40)}...`
-												: appointments.subject}
-										</p>
-									</td>
-									<td className="text-center">
-										<div
-											className={`w-24 h-5 badge badge-xs ${
-												appointments && appointments.status === "Pending"
-													? "badge-warning"
-													: appointments &&
-													  appointments.status === "Responded"
-													? "badge-success"
-													: appointments &&
-													  appointments.status === "Appointed"
-													? "badge-info"
-													: ""
-											}`}
-										>
-											{appointments.status}
-										</div>
-									</td>
+  // Calculate the index range of appointment to display for the current page
+  const indexOfLastInquiry = currentPage * AppointmentPerPage;
+  const indexOfFirstInquiry = indexOfLastInquiry - AppointmentPerPage;
+  const currentAppointments = appointments?.slice(
+    indexOfFirstInquiry,
+    indexOfLastInquiry
+  );
 
-									{/* Delete and Edit */}
-									<td>
-										<div className="flex flex-row justify-center items-center gap-x-5">
-											<button
-												className="btn btn-xs"
-												onClick={(e) => {
-													// Stop event propagation to prevent row hover effect
-													e.stopPropagation();
-													showDeleteModal(appointments.id);
-												}}
-											>
-												Delete
-											</button>
-											<button className="btn btn-xs text-green-700">
-												Edit
-											</button>
-										</div>
-									</td>
-								</tr>
-							))}
-						</tbody>
-					</table>
+  return (
+    <div className="min-h-screen w-full">
+      {/* navigation bar */}
+      <Navbar userType="counselor" />
 
-					{/* Pagination controls */}
-					<div className="join pt-5">
-						<button
-							onClick={() => setCurrentPage(currentPage - 1)}
-							disabled={currentPage === 1}
-							className="join-item btn w-28"
-						>
-							Previous
-						</button>
+      {/* header */}
+      <div className="w-full h-[55vh] relative">
+        {/* Background image */}
+        <div
+          className="absolute inset-0 bg-cover bg-center opacity-40"
+          style={{
+            backgroundImage: `url(${hdrAppointment.src})`,
+          }}
+        ></div>
 
-						{appointments &&
-							[...Array(Math.ceil(appointments.length / AppointmentPerPage))].map(
-								(_, index) => (
-									<button
-										key={index}
-										className={`join-item btn ${
-											currentPage === index + 1 ? "btn-active" : ""
-										}`}
-										onClick={() => setCurrentPage(index + 1)}
-									>
-										{index + 1}
-									</button>
-								)
-							)}
+        {/* Content */}
+        <div className="relative z-10 flex items-center justify-center h-full">
+          <div className="flex flex-col text-left px-44 py-10 gap-y-4">
+            <h1 className="font-Merriweather text-8xl">Appointments</h1>
+            <p className="w-1/2 font-Jaldi text-xl">
+              Manage sessions effortlessly and provide tailored guidance and
+              support to students through efficient booking and coordination.
+              Streamline your scheduling process and ensure students receive
+              personalized attention.
+            </p>
+          </div>
+        </div>
+      </div>
 
-						<button
-							onClick={() => setCurrentPage(currentPage + 1)}
-							disabled={AppointmentPerPage > appointments.length}
-							className="join-item btn w-28"
-						>
-							Next
-						</button>
-					</div>
-				</div>
-			</div>
+      <div className="flex flex-col text-center">
+        {/* table*/}
+        <div className="overflow-x-auto px-56 py-10 ">
+          <table className="table bg-gray-100">
+            {/* head */}
+            <thead>
+              <tr className="bg-gray-200 font-bold">
+                <th className="text-center p-5">ID</th>
+                <th>Date and Time</th>
+                <th className="p-5">ID Number</th>
+                <th>Student</th>
+                <th className="">Reason</th>
+                <th className="text-center">Status</th>
+                {/* Delete and Edit*/}
+                <th className="no-hover-highlight"></th>
+              </tr>
+            </thead>
+            <tbody>
+              {currentAppointments?.map((appointments) => (
+                <tr
+                  key={appointments.appointmentId}
+                  onClick={() => handleRowClick(appointments.appointmentId)}
+                  className="cursor-pointer hover:bg-gray-200 transition duration-300 ease-in-out"
+                >
+                  <td className="text-center">{appointments.appointmentId}</td>
+                  <td>
+                    <div className="flex flex-row gap-x-3">
+                      <div className="text-sm">
+                        {formatDate(appointments.date)} {appointments.timeStart}
+                        -{appointments.timeEnd}
+                      </div>
+                    </div>
+                  </td>
+                  <td>
+                    <div className="flex flex-row gap-x-3">
+                      <div>{appointments.student.idNumber}</div>
+                    </div>
+                  </td>
+                  <td>
+                    <div className="flex items-center gap-3">
+                      <div className="avatar">
+                        <div className="mask mask-squircle w-12 h-12">
+                          <img
+                            src={appointments.student.image}
+                            alt="Avatar Tailwind CSS Component"
+                          />
+                        </div>
+                      </div>
+                      <div>
+                        <div className="font-bold">
+                          {appointments.student.firstName}{" "}
+                          {appointments.student.lastName}
+                        </div>
+                        <div className="text-sm opacity-50">
+                          {appointments.student.institutionalEmail}
+                        </div>
+                      </div>
+                    </div>
+                  </td>
+                  <td>
+                    <p>
+                      {appointments.purpose.length > 50
+                        ? `${appointments.purpose.substring(0, 40)}...`
+                        : appointments.purpose}
+                    </p>
+                  </td>
+                  <td className="text-center">
+                    <div
+                      className={`w-24 h-5 badge badge-xs ${
+                        appointments && appointments.status === false
+                          ? "badge-warning"
+                          : appointments && appointments.status === true
+                          ? "badge-success"
+                          : ""
+                      }`}
+                    >
+                      {appointments.status ? "Done" : "Pending"}
+                    </div>
+                  </td>
 
-			{/* modals */}
-			{deleteModal && (
-				<ModalDelete
-					setDeleteModal={setDeleteModal}
-					handleDelete={handleDelete}
-				></ModalDelete>
-			)}
+                  {/* Delete and Edit */}
+                  <td>
+                    <div className="flex flex-row justify-center items-center gap-x-5">
+                      <button
+                        className="btn btn-xs"
+                        onClick={(e) => {
+                          // Stop event propagation to prevent row hover effect
+                          e.stopPropagation();
+                          showDeleteModal(appointments.id);
+                        }}
+                      >
+                        Delete
+                      </button>
+                      <button className="btn btn-xs text-green-700">
+                        Edit
+                      </button>
+                    </div>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
 
-			{appointmentModal && (
-				<ModalAppointmentInfo
-					setAppointmentModal={setAppointmentModal}
-					selectedID={selectedID}
-					appointments={appointments}
+          {/* Pagination controls */}
+          <div className="join pt-5">
+            <button
+              onClick={() => setCurrentPage(currentPage - 1)}
+              disabled={currentPage === 1}
+              className="join-item btn w-28"
+            >
+              Previous
+            </button>
 
-					// TO BE ADDED
-					// handleRescedule={handleReschedule}
-					// handleUpdateStatus={handleUpdateStatus}
-				></ModalAppointmentInfo>
-			)}
-		</div>
-	);
+            {appointments &&
+              [
+                ...Array(Math.ceil(appointments.length / AppointmentPerPage)),
+              ].map((_, index) => (
+                <button
+                  key={index}
+                  className={`join-item btn ${
+                    currentPage === index + 1 ? "btn-active" : ""
+                  }`}
+                  onClick={() => setCurrentPage(index + 1)}
+                >
+                  {index + 1}
+                </button>
+              ))}
+
+            <button
+              onClick={() => setCurrentPage(currentPage + 1)}
+              disabled={AppointmentPerPage > appointments.length}
+              className="join-item btn w-28"
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      </div>
+
+      {/* modals */}
+      {deleteModal && (
+        <ModalDelete
+          setDeleteModal={setDeleteModal}
+          handleDelete={handleDelete}
+        ></ModalDelete>
+      )}
+
+      {appointmentModal && (
+        <ModalAppointmentInfo
+          setAppointmentModal={setAppointmentModal}
+          selectedID={selectedID}
+          appointments={appointments}
+
+          // TO BE ADDED
+          // handleRescedule={handleReschedule}
+          // handleUpdateStatus={handleUpdateStatus}
+        ></ModalAppointmentInfo>
+      )}
+    </div>
+  );
 }
--- components/ui/modals/counselor/appointments/ModalAppointmentInfo.jsx
+++ components/ui/modals/counselor/appointments/ModalAppointmentInfo.jsx
@@ -7,127 +7,131 @@ import FullButton from "@/components/ui/buttons/FullButton";
 import "@/styles/counselor.css";
 
 const ModalAppointmentInfo = ({
-	setAppointmentModal,
-	selectedID,
-	appointments,
+  setAppointmentModal,
+  selectedID,
+  appointments,
 }) => {
-	const [isChecked, setIsChecked] = useState(true);
-	const [appointment, setAppointment] = useState(null);
+  const [isChecked, setIsChecked] = useState(true);
+  const [appointment, setAppointment] = useState(null);
 
-	// for dialog
-	const toggleChecked = () => {
-		setIsChecked(!isChecked);
-	};
+  // for dialog
+  const toggleChecked = () => {
+    setIsChecked(!isChecked);
+  };
 
-	// find appointments
-	useEffect(() => {
-		const handleFindAppointment = () => {
-			const foundAppointment = appointments.find(
-				(appointment) => appointment.id === selectedID
-			);
-			setAppointment(foundAppointment);
-		};
+  // find appointments
+  useEffect(() => {
+    const handleFindAppointment = () => {
+      const foundAppointment = appointments.find(
+        (appointment) => appointment.appointmentId === selectedID
+      );
+      setAppointment(foundAppointment);
+    };
 
-		handleFindAppointment();
-	}, [selectedID, appointments]);
+    handleFindAppointment();
+  }, [selectedID, appointments]);
 
-	// handle response
-	const handleResponse = () => {
-		console.log(response);
+  const formatDate = (date) => {
+    const dateObject = new Date(date);
+    const options = { year: "numeric", month: "long", day: "numeric" };
+    return dateObject.toLocaleDateString("en-US", options);
+  };
 
-		// logic to update appointment response, mollaeyo
+  // handle response
+  const handleResponse = () => {
+    console.log(response);
 
-		// set appointment.status to "Responded", "Pending", "Appointed"
-	};
+    // logic to update appointment response, mollaeyo
 
-	return (
-		<>
-			<input
-				type="checkbox"
-				id="my_modal_7"
-				className="modal-toggle"
-				checked={isChecked}
-				onChange={toggleChecked}
-			/>
-			<div className="modal" role="dialog">
-				<div className="modal-box p-9 text-left max-w-2xl max-h-fit">
-					<img
-						src={iconDelete.src} // change to setter avatar
-						alt="setter avatar"
-						className="w-24 h-24 flex justify-center mx-auto"
-					/>
+    // set appointment.status to "Responded", "Pending", "Appointed"
+  };
 
-					<table className="my-4">
-						<tbody>
-							<tr>
-								<th>ID Number:</th>
-								<td>
-									{appointment
-										? appointment.setter.idNumber
-										: ""}
-								</td>
-							</tr>
-							<tr>
-								<th>Name:</th>
-								<td>
-									{appointment ? appointment.setter.name : ""}
-								</td>
-							</tr>
-							<tr>
-								<th>Purpose:</th>
-								<td>
-									{appointment ? appointment.subject : ""}
-								</td>
-							</tr>
-							<tr>
-								<th>Addtional Notes:</th>
-								<td>
-									{appointment ? appointment.details : ""}
-								</td>
-							</tr>
-							<tr>
-								<th>Date:</th>
-								<td>{appointment ? appointment.date : ""}</td>
-							</tr>
-							<tr>
-								<th>Time:</th>
-								<td>{appointment ? appointment.time : ""}</td>
-							</tr>
-							<tr>
-								<th>Status:</th>
-								<td
-									className={`h-fit badge badge-md ${
-										appointment?.status === "Pending"
-											? "badge-warning"
-											: "badge-success"
-									}`}
-									style={{ width: "30%" }}
-								>
-									{appointment ? appointment.status : ""}
-								</td>
-							</tr>
-						</tbody>
-					</table>
+  return (
+    <>
+      <input
+        type="checkbox"
+        id="my_modal_7"
+        className="modal-toggle"
+        checked={isChecked}
+        onChange={toggleChecked}
+      />
+      <div className="modal" role="dialog">
+        <div className="modal-box p-9 text-left max-w-2xl max-h-fit">
+          <img
+            src={iconDelete.src} // change to setter avatar
+            alt="setter avatar"
+            className="w-24 h-24 flex justify-center mx-auto"
+          />
 
-					<div className="flex flex-row gap-x-4 mt-6 px-14">
-						<HollowButton onClick={() => setConfirmResponse(true)}>
-							Reschedule
-						</HollowButton>
-						<FullButton onClick={() => setConfirmResponse(true)}>
-							Update Status
-						</FullButton>
-					</div>
-				</div>
-				<label
-					className="modal-backdrop"
-					htmlFor="my_modal_7"
-					onClick={() => setAppointmentModal(false)}
-				>
-					Close
-				</label>
-			</div>
-		</>
-	);
+          <table className="my-4">
+            <tbody>
+              <tr>
+                <th>ID Number:</th>
+                <td>{appointment ? appointment.student.idNumber : ""}</td>
+              </tr>
+              <tr>
+                <th>Name:</th>
+                <td>
+                  {appointment
+                    ? `${appointment.student.firstName} ${appointment.student.lastName}`
+                    : ""}
+                </td>
+              </tr>
+              <tr>
+                <th>Purpose:</th>
+                <td>{appointment ? appointment.purpose : ""}</td>
+              </tr>
+              <tr>
+                <th>Addtional Notes:</th>
+                <td>{appointment ? appointment.additionalNotes : ""}</td>
+              </tr>
+              <tr>
+                <th>Date:</th>
+                <td>{appointment ? formatDate(appointment.date) : ""}</td>
+              </tr>
+              <tr>
+                <th>Time:</th>
+                <td>
+                  {appointment
+                    ? `${appointment.timeStart}-${appointment.timeEnd}`
+                    : ""}
+                </td>
+              </tr>
+              <tr>
+                <th>Status:</th>
+                <td
+                  className={`h-fit badge badge-md ${
+                    appointment?.status === false
+                      ? "badge-warning"
+                      : "badge-success"
+                  }`}
+                  style={{ width: "30%" }}
+                >
+                  {appointment ? (appointment.status ? "Done" : "Pending") : ""}
+                </td>
+              </tr>
+            </tbody>
+          </table>
+
+          <div className="flex flex-row gap-x-4 mt-6 px-14">
+            <HollowButton onClick={() => setConfirmResponse(true)}>
+              Reschedule
+            </HollowButton>
+            <FullButton onClick={() => setConfirmResponse(true)}>
+              Update Status
+            </FullButton>
+          </div>
+        </div>
+        <label
+          className="modal-backdrop"
+          htmlFor="my_modal_7"
+          onClick={() => setAppointmentModal(false)}
+        >
+          Close
+        </label>
+      </div>
+    </>
+  );
 };
 
 export default ModalAppointmentInfo;
