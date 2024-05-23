0654a58d8f0606c2b41a47e059953d1219065e39
showah <146901959+showah@users.noreply.github.com>
2024-05-23 21:08:03 +0800

    partial counselor landing page

--- app/counselor/page.jsx
+++ app/counselor/page.jsx
@@ -1,50 +1,33 @@
 "use client";
 
-import { useSession, signOut } from "next-auth/react";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useState } from "react";
+
+import SideNav from "@/components/admin_components/SideNav";
+import Box from "@mui/material/Box";
+import { DrawerHeader } from "@/components/admin_components/SideNav";
+import Dashboard from "@/components/admin_components/pages/Dashboard";
+import Referrals from "@/components/admin_components/pages/Referrals";
+import Users from "@/components/admin_components/pages/Users";
+import Appointments from "@/components/admin_components/pages/Appointments";
 import { Navbar } from "@/components/ui/Navbar";
-import Loading from "@/components/Skeleton";
-const CounselorPage = () => {
-  const { data: session, status } = useSession();
-  const router = useRouter();
-
-  // useEffect(() => {
-  //   if (status === "unauthenticated") router.push("/login");
-  // }, [status]);
-
-  // if (status === "loading" || !session) {
-  //   return <Loading />;
-  // }
-
-  // // Redirect authenticated users who are not counselors
-  // if (session.user.role !== "counselor") {
-  //   router.push("/login"); // Redirect to homepage or appropriate page
-  //   return null; // Prevent rendering anything if redirecting
-  // }
-
-  // console.log(session);
+const AdminDashboard = () => {
+  const [page, setPage] = useState("Dashboard");
   return (
-    <div className="w-full h-screen flex flex-col items-center justify-center">
-      <Navbar userType="counselor" />
-      <h1>Counselor Page</h1>
-      <button
-        className="pointer px-4 py-8"
-        onClick={() => signOut({ callbackUrl: "/login", redirect: true })}
+    <div className="w-full min-h-screen flex bg-white">
+      
+      <Box
+        component="main"
+        sx={{ flexGrow: 1, p: 3, backgroundColor: "white" }}
       >
-        Logout
-      </button>
-      <p>User Data</p>
-      <img src={session?.user.image} />
-      <p>User id (in db): {session?.user.id}</p>
-      <p>Name: {session?.user.name}</p>
-      <p>Gender: {session?.user.gender}</p>
-      <p>School Id: {session?.user.idNumber}</p>
-      <p>Institutional email: {session?.user.email}</p>
-      <p>Password (hashed): {session?.user.password}</p>
-      <p>Role: {session?.user.role}</p>
+        <Navbar userType="counselor" />
+        <DrawerHeader />
+        {page === "Dashboard" && <Dashboard />}
+        {page === "Appointments" && <Appointments />}
+        {page === "Users" && <Users />}
+        {page === "Referrals" && <Referrals />}
+      </Box>
     </div>
   );
 };
 
-export default CounselorPage;
+export default AdminDashboard;
