1d84ed71401bdfbde58491e160c9cd32c30be224
keisamae <kheisa.selma@cit.edu>
2024-05-23 12:51:50 +0800

    integrate student profile

--- app/student/profile/page.jsx
+++ app/student/profile/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useSession } from "next-auth/react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import { Navbar } from "@/components/ui/Navbar";
 import FullButton from "@/components/ui/buttons/FullButton";
@@ -11,121 +11,154 @@ import Birthdate from "@/components/ui/student/Birthdate";
 import TextDisplay from "@/components/ui/student/TextDisplay";
 
 export default function StudentProfile() {
-	const [showModal, setShowModal] = useState(false);
-	const { data: session } = useSession();
-	console.log(session);
+  const [showModal, setShowModal] = useState(false);
+  const { data: session } = useSession();
+  const [studentProfile, setStudentProfile] = useState(null);
 
-	const handleUpdateProfile = () => {
-		setShowModal(true);
-	};
+  useEffect(() => {
+    const fetchStudentProfile = async () => {
+      try {
+        const response = await fetch(
+          "/api/users/viewuser/view-student-via-id?studentId=" +
+            session?.user.id,
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+        const data = await response.json();
+        setStudentProfile(data.student);
+      } catch (error) {
+        console.log("Error fetching student profile");
+      }
+    };
 
-	const handleCloseModal = () => {
-		setShowModal(false);
-	};
+    fetchStudentProfile();
+  }, []);
 
-	return (
-		<div>
-			<section>
-				<Navbar userType="student" />
-			</section>
+  const formatTime = (time) => {
+    return new Date(time).toLocaleDateString("en-US", {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    });
+  };
 
-			<section className="mt-20 px-72 py-20">
-				<section className="w-full h-fit flex flex-row items-center gap-x-10">
-					{/* Main Identification */}
-					<div className="w-2/12 h-full flex items-center avatar">
-						<div className="w-48 rounded-full ring ring-[#6B9080] ring-offset-base-100 ring-offset-2">
-							<img src={session?.user.image} />
-						</div>
-					</div>
-					<div className=" w-10/12 h-full flex flex-col justify-center">
-						<h1 className="font-Merriweather text-4xl font-thin tracking-tight">
-							Hello, {session?.user.name}
-						</h1>
-						<p className="font-Merriweather tracking-tight font-thin my-2">
-							{session?.user.email}
-						</p>
-						<div className="w-2/12 mt-1">
-							<FullButton onClick={handleUpdateProfile}>
-								Update Profile
-							</FullButton>
-						</div>
-					</div>
-				</section>
+  const handleUpdateProfile = () => {
+    setShowModal(true);
+  };
 
-				{/* Profile Details */}
-				<section className="flex flex-col gap-y-10 py-16">
-					{/* Name */}
-					<div>
-						<div className="w-full flex flex-row gap-x-6">
-							<div className="w-5/12">
-								<TextDisplay
-									label="First Name"
-									value={session?.user.name}
-								/>
-							</div>
-							<div className="w-5/12">
-								<TextDisplay
-									label="Last Name"
-									value={session?.user.lastname}
-								/>
-							</div>
-							<div className="w-2/12">
-								<TextDisplay
-									label="Gender"
-									value={session?.user.gender}
-								/>
-							</div>
-						</div>
-					</div>
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
 
-					{/* College Information */}
-					<div>
-						<div className="w-full flex flex-row gap-x-6">
-							<div className="w-5/12">
-								<TextDisplay
-									label="ID Number"
-									value={session?.user.idNumber}
-								/>
-							</div>
-							<div className="w-5/12">
-								<TextDisplay
-									label="Program"
-									value={session?.user.program}
-								/>
-							</div>
-							<div className="w-2/12">
-								<TextDisplay
-									label="Year Level"
-									value={session?.user.year}
-								/>
-							</div>
-						</div>
-					</div>
+  return (
+    <div>
+      <section>
+        <Navbar userType="student" />
+      </section>
 
-					{/* Additional Details */}
-					<div>
-						<div className="w-full flex flex-row gap-x-6">
-							<div className="w-3/12">
-								<Birthdate value="2023-05-16" />
-							</div>
-							<div className="w-3/12">
-								<TextDisplay
-									label="Contact Number"
-									value={session?.user.program}
-								/>
-							</div>
-							<div className="w-6/12">
-								<TextDisplay
-									label="Address"
-									value={session?.user.year}
-								/>
-							</div>
-						</div>
-					</div>
-				</section>
-			</section>
+      {studentProfile && (
+        <section className="mt-20 px-72 py-20">
+          <section className="w-full h-fit flex flex-row items-center gap-x-10">
+            {/* Main Identification */}
+            <div className="w-2/12 h-full flex items-center avatar">
+              <div className="w-48 rounded-full ring ring-[#6B9080] ring-offset-base-100 ring-offset-2">
+                <img src={session?.user.image} />
+              </div>
+            </div>
+            <div className=" w-10/12 h-full flex flex-col justify-center">
+              <h1 className="font-Merriweather text-4xl font-thin tracking-tight">
+                Hello, {studentProfile.user.firstName}{" "}
+                {studentProfile.user.lastName}
+              </h1>
+              <p className="font-Merriweather tracking-tight font-thin my-2">
+                {studentProfile.user.institutionalEmail}
+              </p>
+              <div className="w-2/12 mt-1">
+                <FullButton onClick={handleUpdateProfile}>
+                  Update Profile
+                </FullButton>
+              </div>
+            </div>
+          </section>
 
-			{showModal && <UpdateProfileModal onClose={handleCloseModal} />}
-		</div>
-	);
+          {/* Profile Details */}
+          <section className="flex flex-col gap-y-10 py-16">
+            {/* Name */}
+            <div>
+              <div className="w-full flex flex-row gap-x-6">
+                <div className="w-5/12">
+                  <TextDisplay
+                    label="First Name"
+                    value={studentProfile.user.firstName}
+                  />
+                </div>
+                <div className="w-5/12">
+                  <TextDisplay
+                    label="Last Name"
+                    value={studentProfile.user.lastName}
+                  />
+                </div>
+                <div className="w-2/12">
+                  <TextDisplay
+                    label="Gender"
+                    value={studentProfile.user.gender}
+                  />
+                </div>
+              </div>
+            </div>
+
+            {/* College Information */}
+            <div>
+              <div className="w-full flex flex-row gap-x-6">
+                <div className="w-5/12">
+                  <TextDisplay
+                    label="ID Number"
+                    value={studentProfile.user.idNumber}
+                  />
+                </div>
+                <div className="w-5/12">
+                  <TextDisplay label="Program" value={studentProfile.program} />
+                </div>
+                <div className="w-2/12">
+                  <TextDisplay label="Year Level" value={studentProfile.year} />
+                </div>
+              </div>
+            </div>
+
+            {/* Additional Details */}
+            <div>
+              <div className="w-full flex flex-row gap-x-6">
+                <div className="w-3/12">
+                  <TextDisplay
+                    label="Birth Date"
+                    value={formatTime(studentProfile.birthdate)}
+                  />{" "}
+                </div>
+                <div className="w-3/12">
+                  <TextDisplay
+                    label="Contact Number"
+                    value={studentProfile.contactNumber}
+                  />
+                </div>
+                <div className="w-6/12">
+                  <TextDisplay label="Address" value={studentProfile.address} />
+                </div>
+              </div>
+            </div>
+          </section>
+        </section>
+      )}
+
+      {showModal && (
+        <UpdateProfileModal
+          onClose={handleCloseModal}
+          student={studentProfile}
+        />
+      )}
+    </div>
+  );
 }
--- components/ui/modals/student/UpdateProfileModal.jsx
+++ components/ui/modals/student/UpdateProfileModal.jsx
@@ -6,276 +6,303 @@ import Birthdate from "../../student/Birthdate";
 import TextDisplay from "../../student/TextDisplay";
 import HollowButton from "../../buttons/HollowButton";
 
-const UpdateProfileModal = ({ onClose }) => {
-	const { data: session } = useSession();
+const UpdateProfileModal = ({ onClose, student }) => {
+  const { data: session } = useSession();
 
-	const [tabSelected, setTabSelected] = useState("Account Settings");
+  const [tabSelected, setTabSelected] = useState("Account Settings");
 
-	const [passwords, setPasswords] = useState({
-		currentPassword: "",
-		newPassword: "",
-		confirmPassword: "",
-	});
+  const [passwords, setPasswords] = useState({
+    currentPassword: "",
+    newPassword: "",
+    confirmPassword: "",
+  });
 
-	const [showInvalidPassword, setShowInvalidPassword] = useState({
-		currentPassword: false,
-		newPassword: false,
-		confirmPassword: false,
-	});
+  const [showInvalidPassword, setShowInvalidPassword] = useState({
+    currentPassword: false,
+    newPassword: false,
+    confirmPassword: false,
+  });
 
-	const handleTabSelection = (tab) => {
-		setTabSelected(tab);
-	};
+  const handleTabSelection = (tab) => {
+    setTabSelected(tab);
+  };
 
-	// password validation function
-	const validatePassword = (password) => {
-		const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-		return regex.test(password);
-	};
+  // password validation function
+  const validatePassword = (password) => {
+    const regex =
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    return regex.test(password);
+  };
 
-	const handlePasswordChange = (label) => (e) => {
-		const pw = e.target.value;
+  const handlePasswordChange = (label) => (e) => {
+    const pw = e.target.value;
 
-		setPasswords((prevPasswords) => ({
-			...prevPasswords,
-			[label]: pw,
-		}));
+    setPasswords((prevPasswords) => ({
+      ...prevPasswords,
+      [label]: pw,
+    }));
 
-		setShowInvalidPassword((prevShowInvalidPassword) => ({
-			...prevShowInvalidPassword,
-			[label]: pw && !validatePassword(pw),
-		}));
-	};
+    setShowInvalidPassword((prevShowInvalidPassword) => ({
+      ...prevShowInvalidPassword,
+      [label]: pw && !validatePassword(pw),
+    }));
+  };
 
-	const handleSelectPhoto = () => {
-		console.log("Select Photo");
-	};
+  const handleSelectPhoto = () => {
+    console.log("Select Photo");
+  };
 
-	const handleSave = () => {
-		console.log(passwords);
-	};
+  const handleSave = () => {
+    console.log(passwords);
+  };
 
-	const handleCancel = () => {
-        onClose(); // Call the onClose function when Cancel button is clicked
-    };
-	return (
-		<>
-			<input type="checkbox" id="my_modal_7" className="modal-toggle" checked={true} />
-			<div className="modal" role="dialog">
-				<div
-					className="w-full modal-box p-8"
-					style={{ maxWidth: "65vw", minHeight: "90vh" }}
-				>
-					<TabSelection handleTabSelection={handleTabSelection} />
+  const handleCancel = () => {
+    onClose(); // Call the onClose function when Cancel button is clicked
+  };
+  return (
+    <>
+      <input
+        type="checkbox"
+        id="my_modal_7"
+        className="modal-toggle"
+        checked={true}
+      />
+      <div className="modal" role="dialog">
+        <div
+          className="w-full modal-box p-8"
+          style={{ maxWidth: "65vw", minHeight: "90vh" }}
+        >
+          <TabSelection handleTabSelection={handleTabSelection} />
 
-					<section className="flex flex-col items-center">
-						<AvatarDisplay session={session} handleSelectPhoto={handleSelectPhoto} />
+          <section className="flex flex-col items-center">
+            <AvatarDisplay
+              student={student}
+              handleSelectPhoto={handleSelectPhoto}
+            />
 
-						{tabSelected === "Account Settings" && <AccountTab session={session} />}
+            {tabSelected === "Account Settings" && (
+              <AccountTab student={student} />
+            )}
 
-						{tabSelected === "Security Settings" && (
-							<SecurityTab
-								passwords={passwords}
-								handlePasswordChange={handlePasswordChange}
-								showInvalidPassword={showInvalidPassword}
-							/>
-						)}
+            {tabSelected === "Security Settings" && (
+              <SecurityTab
+                passwords={passwords}
+                handlePasswordChange={handlePasswordChange}
+                showInvalidPassword={showInvalidPassword}
+              />
+            )}
 
-						<Save onClick={handleSave} />
-					</section>
-				</div>
-				<label className="modal-backdrop" htmlFor="my_modal_7" onClick={() => onClose()}>
-					Close
-				</label>
-			</div>
-		</>
-	);
+            <Save onClick={handleSave} />
+          </section>
+        </div>
+        <label
+          className="modal-backdrop"
+          htmlFor="my_modal_7"
+          onClick={() => onClose()}
+        >
+          Close
+        </label>
+      </div>
+    </>
+  );
 };
 
 export default UpdateProfileModal;
 
 const TabSelection = ({ handleTabSelection }) => {
-	return (
-		<section className="flex flex-row gap-x-3">
-			<div
-				className="outline-0 bg-gray-200 w-1/4 h-12 rounded-xl flex items-center justify-center font-semibold hover:scale-95"
-				onClick={() => handleTabSelection("Account Settings")}
-			>
-				Account Settings
-			</div>
-			<div
-				className="outline-0 bg-gray-200 w-1/4 h-12 rounded-xl flex items-center justify-center font-semibold hover:scale-95"
-				onClick={() => handleTabSelection("Security Settings")}
-			>
-				Security Settings
-			</div>
-		</section>
-	);
+  return (
+    <section className="flex flex-row gap-x-3">
+      <div
+        className="outline-0 bg-gray-200 w-1/4 h-12 rounded-xl flex items-center justify-center font-semibold hover:scale-95"
+        onClick={() => handleTabSelection("Account Settings")}
+      >
+        Account Settings
+      </div>
+      <div
+        className="outline-0 bg-gray-200 w-1/4 h-12 rounded-xl flex items-center justify-center font-semibold hover:scale-95"
+        onClick={() => handleTabSelection("Security Settings")}
+      >
+        Security Settings
+      </div>
+    </section>
+  );
 };
 
-const AvatarDisplay = ({ session, handleSelectPhoto }) => {
-	return (
-		<section className="py-8 flex flex-col justify-center items-center gap-y-5 mt-4">
-			<div className="w-2/12 h-full flex flex-col items-center justify-center avatar relative">
-				<div className="w-36 rounded-full ring ring-[#6B9080] ring-offset-base-100 ring-offset-2 relative">
-					<img src={session?.user.image} className="rounded-full" />
-				</div>
-			</div>
-			<button
-				className="h-fit bg-[#577c6c] text-white p-2 px-5 rounded-full font-Merriweather text-sm"
-				onClick={handleSelectPhoto}
-			>
-				Select Photo
-			</button>
-		</section>
-	);
+const AvatarDisplay = ({ student, handleSelectPhoto }) => {
+  return (
+    <section className="py-8 flex flex-col justify-center items-center gap-y-5 mt-4">
+      <div className="w-2/12 h-full flex flex-col items-center justify-center avatar relative">
+        <div className="w-36 rounded-full ring ring-[#6B9080] ring-offset-base-100 ring-offset-2 relative">
+          <img src={student.user.image} className="rounded-full" />
+        </div>
+      </div>
+      <button
+        className="h-fit bg-[#577c6c] text-white p-2 px-5 rounded-full font-Merriweather text-sm"
+        onClick={handleSelectPhoto}
+      >
+        Select Photo
+      </button>
+    </section>
+  );
 };
 
-const AccountTab = ({ session }) => {
-	return (
-		<section className="h-full w-full px-20 flex flex-col items-center">
-			{/* Profile Details */}
-			<section className="w-full flex flex-col gap-y-5 py-2">
-				{/* Name */}
-				<div>
-					<div className="w-full flex flex-row gap-x-6">
-						<div className="w-5/12">
-							<TextDisplay label="First Name" value={session?.user.name} />
-						</div>
-						<div className="w-5/12">
-							<TextDisplay label="Last Name" value={session?.user.lastname} />
-						</div>
-						<div className="w-2/12">
-							<TextDisplay label="Gender" value={session?.user.gender} />
-						</div>
-					</div>
-				</div>
-				{/* College Information */}
-				<div>
-					<div className="w-full flex flex-row gap-x-6">
-						<div className="w-5/12">
-							<TextDisplay label="ID Number" value={session?.user.idNumber} />
-						</div>
-						<div className="w-5/12">
-							<TextDisplay label="Program" value={session?.user.program} />
-						</div>
-						<div className="w-2/12">
-							<TextDisplay label="Year Level" value={session?.user.year} />
-						</div>
-					</div>
-				</div>
-				{/* Additional Details */}
-				<div>
-					<div className="w-full flex flex-row gap-x-6">
-						<div className="w-3/12">
-							<Birthdate value="2023-05-16" />
-						</div>
-						<div className="w-3/12">
-							<TextDisplay label="Contact Number" value={session?.user.program} />
-						</div>
-						<div className="w-6/12">
-							<TextDisplay label="Address" value={session?.user.year} />
-						</div>
-					</div>
-				</div>
-			</section>
-		</section>
-	);
+const AccountTab = ({ student }) => {
+  return (
+    <section className="h-full w-full px-20 flex flex-col items-center">
+      {/* Profile Details */}
+      <section className="w-full flex flex-col gap-y-5 py-2">
+        {/* Name */}
+        <div>
+          <div className="w-full flex flex-row gap-x-6">
+            <div className="w-5/12">
+              <TextDisplay label="First Name" value={student.user.firstName} />
+            </div>
+            <div className="w-5/12">
+              <TextDisplay label="Last Name" value={student.user.lastName} />
+            </div>
+            <div className="w-2/12">
+              <TextDisplay label="Gender" value={student.user.gender} />
+            </div>
+          </div>
+        </div>
+        {/* College Information */}
+        <div>
+          <div className="w-full flex flex-row gap-x-6">
+            <div className="w-5/12">
+              <TextDisplay label="ID Number" value={student.user.idNumber} />
+            </div>
+            <div className="w-5/12">
+              <TextDisplay label="Program" value={student.program} />
+            </div>
+            <div className="w-2/12">
+              <TextDisplay label="Year Level" value={student.year} />
+            </div>
+          </div>
+        </div>
+        {/* Additional Details */}
+        <div>
+          <div className="w-full flex flex-row gap-x-6">
+            <div className="w-3/12">
+              <Birthdate value="2023-05-16" />
+            </div>
+            <div className="w-3/12">
+              <TextDisplay
+                label="Contact Number"
+                value={student.contactNumber}
+              />
+            </div>
+            <div className="w-6/12">
+              <TextDisplay label="Address" value={student.address} />
+            </div>
+          </div>
+        </div>
+      </section>
+    </section>
+  );
 };
 
-const SecurityTab = ({ passwords, handlePasswordChange, showInvalidPassword }) => {
-	return (
-		<section className="h-full w-full px-20 flex flex-col items-center">
-			<section className="w-full flex flex-col gap-y-5 py-2">
-				{/* Current Password */}
-				<div>
-					<div className="w-full flex flex-row justify-center">
-						<div className="w-5/12">
-							<PasswordInput
-								label="currentPassword"
-								password={passwords.currentPassword}
-								handlePasswordChange={handlePasswordChange}
-								showInvalidPassword={showInvalidPassword.currentPassword}
-							/>
-						</div>
-					</div>
-				</div>
-				{/* New Password */}
-				<div>
-					<div className="w-full flex flex-row gap-x-6 justify-center">
-						<div className="w-5/12">
-							<PasswordInput
-								label="newPassword"
-								password={passwords.newPassword}
-								handlePasswordChange={handlePasswordChange}
-								showInvalidPassword={showInvalidPassword.newPassword}
-							/>
-						</div>
-					</div>
-				</div>
-				{/* Confirm Password */}
-				<div>
-					<div className="w-full flex flex-row gap-x-6 justify-center">
-						<div className="w-5/12">
-							<PasswordInput
-								label="confirmPassword"
-								password={passwords.confirmPassword}
-								handlePasswordChange={handlePasswordChange}
-								showInvalidPassword={showInvalidPassword.confirmPassword}
-							/>
-						</div>
-					</div>
-				</div>
-			</section>
-		</section>
-	);
+const SecurityTab = ({
+  passwords,
+  handlePasswordChange,
+  showInvalidPassword,
+}) => {
+  return (
+    <section className="h-full w-full px-20 flex flex-col items-center">
+      <section className="w-full flex flex-col gap-y-5 py-2">
+        {/* Current Password */}
+        <div>
+          <div className="w-full flex flex-row justify-center">
+            <div className="w-5/12">
+              <PasswordInput
+                label="currentPassword"
+                password={passwords.currentPassword}
+                handlePasswordChange={handlePasswordChange}
+                showInvalidPassword={showInvalidPassword.currentPassword}
+              />
+            </div>
+          </div>
+        </div>
+        {/* New Password */}
+        <div>
+          <div className="w-full flex flex-row gap-x-6 justify-center">
+            <div className="w-5/12">
+              <PasswordInput
+                label="newPassword"
+                password={passwords.newPassword}
+                handlePasswordChange={handlePasswordChange}
+                showInvalidPassword={showInvalidPassword.newPassword}
+              />
+            </div>
+          </div>
+        </div>
+        {/* Confirm Password */}
+        <div>
+          <div className="w-full flex flex-row gap-x-6 justify-center">
+            <div className="w-5/12">
+              <PasswordInput
+                label="confirmPassword"
+                password={passwords.confirmPassword}
+                handlePasswordChange={handlePasswordChange}
+                showInvalidPassword={showInvalidPassword.confirmPassword}
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+    </section>
+  );
 };
 
-const PasswordInput = ({ label, password, handlePasswordChange, showInvalidPassword }) => {
-	const [properLabel, setProperLabel] = useState("");
+const PasswordInput = ({
+  label,
+  password,
+  handlePasswordChange,
+  showInvalidPassword,
+}) => {
+  const [properLabel, setProperLabel] = useState("");
 
-	useEffect(() => {
-		switch (label) {
-			case "currentPassword":
-				setProperLabel("Current Password");
-				break;
-			case "newPassword":
-				setProperLabel("New Password");
-				break;
-			case "confirmPassword":
-				setProperLabel("Confirm New Password");
-				break;
-			default:
-				setProperLabel("");
-		}
-	}, [label]);
+  useEffect(() => {
+    switch (label) {
+      case "currentPassword":
+        setProperLabel("Current Password");
+        break;
+      case "newPassword":
+        setProperLabel("New Password");
+        break;
+      case "confirmPassword":
+        setProperLabel("Confirm New Password");
+        break;
+      default:
+        setProperLabel("");
+    }
+  }, [label]);
 
-	return (
-		<>
-			<div className="w-full full flex flex-col">
-				<TextInput
-					label={properLabel}
-					value={password}
-					onChange={handlePasswordChange(label)}
-					type="password"
-				/>
-				{/* error message */}
-				{showInvalidPassword && password && (
-					<div className="text-red-600 text-xs pt-2 pb-1.5">
-						Choose a password with at least 8 characters, including at least one
-						uppercase letter, one lowercase letter, one number, and one special
-						character.
-					</div>
-				)}
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className="w-full full flex flex-col">
+        <TextInput
+          label={properLabel}
+          value={password}
+          onChange={handlePasswordChange(label)}
+          type="password"
+        />
+        {/* error message */}
+        {showInvalidPassword && password && (
+          <div className="text-red-600 text-xs pt-2 pb-1.5">
+            Choose a password with at least 8 characters, including at least one
+            uppercase letter, one lowercase letter, one number, and one special
+            character.
+          </div>
+        )}
+      </div>
+    </>
+  );
 };
 
 const Save = ({ onClick, handleCancel }) => {
-	return (
-		<section className="mt-9 w-4/12 flex justify-center items-center">
-			<FullButton onClick={onClick}>Save</FullButton>
-		</section>
-	);
+  return (
+    <section className="mt-9 w-4/12 flex justify-center items-center">
+      <FullButton onClick={onClick}>Save</FullButton>
+    </section>
+  );
 };
