7ae0ae67b5390693944c33c375bbe36488e89507
showah <146901959+showah@users.noreply.github.com>
2024-05-24 02:04:03 +0800

    Revert "deleted unnecessary components"
    This reverts commit 16b2767b68a52f70ec62f00ffbb32359842373b5.

---
+++ components/ui/inputs/InputPassword.jsx
@@ -0,0 +1,31 @@
+import TextInput from "@/components/ui/inputs/TextInput";
+import InputFirstPassword from "./InputFirstPassword";
+import InputCheckPassword from "./InputCheckPassword";
+const InputPassword = ({
+	password,
+	passwordCheck,
+	showInvalidPassword,
+	showPasswordDoNotMatch,
+	handlePasswordChange,
+	handlePasswordCheck,
+}) => {
+	return (
+		<>
+			<div className="w-1/2 flex flex-col">
+				<InputFirstPassword
+					password={password}
+					showInvalidPassword={showInvalidPassword}
+					handlePasswordChange={handlePasswordChange}
+				/>
+			</div>
+			<div className="w-1/2 flex flex-col">
+				<InputCheckPassword
+					passwordCheck={passwordCheck}
+					handlePasswordCheck={handlePasswordCheck}
+					showPasswordDoNotMatch={showPasswordDoNotMatch}
+				/>
+			</div>
+		</>
+	);
+};
+export default InputPassword;
