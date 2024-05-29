33802fb821bcd9747568266a1c8ba9bf1ce32c87
joshuabriones <joshua.briones@cit.edu>
2024-05-29 17:04:41 +0800

    admin route protection:

--- app/admin/page.jsx
+++ app/admin/page.jsx
@@ -1,23 +1,45 @@
 "use client";
 
-import { useSession } from "next-auth/react";
+import { useState } from "react";
+
+import SideNav from "@/components/admin_components/SideNav";
+import Box from "@mui/material/Box";
+import { DrawerHeader } from "@/components/admin_components/SideNav";
+import Dashboard from "@/components/admin_components/pages/Dashboard";
+import Referrals from "@/components/admin_components/pages/Referrals";
+import Users from "@/components/admin_components/pages/Users";
+import Appointments from "@/components/admin_components/pages/Appointments";
+import Load from "@/components/Load";
+import Cookies from "js-cookie";
+import { getUserSession } from "@/lib/helperFunctions";
 import { useRouter } from "next/navigation";
 
-const AdminPage = () => {
-  const { data: session } = useSession();
+const AdminDashboard = () => {
+  const userSession = getUserSession();
+  const [page, setPage] = useState("Dashboard");
   const router = useRouter();
 
-  if (!session) {
-    router.push("/login");
-  }
-  if (session && session.user.role !== "admin") {
-    router.push(`/${session.user.role}`);
+  if (Cookies.get("token") === undefined || Cookies.get("token") === null) {
+    return <Load route="login" />;
   }
 
-  if (session && session.user.role === "admin") {
-    router.push("/admin/dashboard");
-  }
-  return <div>Admin Page</div>;
+  if (userSession && userSession.role) return <Load route={userSession.role} />;
+
+  return (
+    <div className="w-full min-h-screen flex bg-white">
+      <SideNav setPage={setPage} />
+      <Box
+        component="main"
+        sx={{ flexGrow: 1, p: 3, backgroundColor: "white" }}
+      >
+        <DrawerHeader />
+        {page === "Dashboard" && <Dashboard />}
+        {page === "Appointments" && <Appointments />}
+        {page === "Users" && <Users />}
+        {page === "Referrals" && <Referrals />}
+      </Box>
+    </div>
+  );
 };
 
-export default AdminPage;
+export default AdminDashboard;
