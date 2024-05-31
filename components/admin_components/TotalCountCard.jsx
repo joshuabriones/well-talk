11120a93a071327053ccad65dc0781b0aa4f161f
joshuabriones <joshua.briones@cit.edu>
2024-05-31 19:02:30 +0800

    admin - total counts card integrated

--- components/admin_components/TotalCountCard.jsx
+++ components/admin_components/TotalCountCard.jsx
@@ -1,32 +1,97 @@
 import Cookies from "js-cookie";
+import { useEffect, useState } from "react";
+import { API_ENDPOINT } from "@/lib/api";
 
 const TotalCountCard = ({ userSession }) => {
-  const totalCountData = [
-    {
-      title: "Total Students",
-      count: 312,
-      icon: "/student-ad.png",
-      bg: "bg-gradient-red-card",
-    },
-    {
-      title: "Total Teachers",
-      count: 21,
-      icon: "/teacher-ad.png",
-      bg: "bg-gradient-green-card",
-    },
-    {
-      title: "Total Counselors",
-      count: 5,
-      icon: "/counselor-da.png",
-      bg: "bg-gradient-blue-card",
-    },
-    {
-      title: "Total Appointments",
-      count: 300,
-      icon: "/calendar-ad.png",
-      bg: "bg-gradient-purple-card",
-    },
-  ];
+  const [totalCountData, setTotalCountData] = useState([{}]);
+  // const totalCountData = [
+  //   {
+  //     title: "Total Students",
+  //     count: 312,
+  //     icon: "/student-ad.png",
+  //     bg: "bg-gradient-red-card",
+  //   },
+  //   {
+  //     title: "Total Teachers",
+  //     count: 21,
+  //     icon: "/teacher-ad.png",
+  //     bg: "bg-gradient-green-card",
+  //   },
+  //   {
+  //     title: "Total Counselors",
+  //     count: 5,
+  //     icon: "/counselor-da.png",
+  //     bg: "bg-gradient-blue-card",
+  //   },
+  //   {
+  //     title: "Total Appointments",
+  //     count: 300,
+  //     icon: "/calendar-ad.png",
+  //     bg: "bg-gradient-purple-card",
+  //   },
+  // ];
+
+  const fetchUsers = async () => {
+    const response = await fetch(
+      `${process.env.BASE_URL}${API_ENDPOINT.GET_ALL_USERS}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${Cookies.get("token")}`,
+        },
+      }
+    );
+    const data = await response.json();
+    setTotalCountData([
+      {
+        title: "Total Students",
+        count: data.filter((user) => user.role === "student").length,
+        icon: "/student-ad.png",
+        bg: "bg-gradient-red-card",
+      },
+      {
+        title: "Total Teachers",
+        count: data.filter((user) => user.role === "teacher").length,
+        icon: "/teacher-ad.png",
+        bg: "bg-gradient-green-card",
+      },
+      {
+        title: "Total Counselors",
+        count: data.filter((user) => user.role === "counselor").length,
+        icon: "/counselor-da.png",
+        bg: "bg-gradient-blue-card",
+      },
+    ]);
+  };
+
+  const fetchAppointments = async () => {
+    const appointments = await fetch(
+      `${process.env.BASE_URL}${API_ENDPOINT.STUDENT_GET_ALL_APPOINTMENTS}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${Cookies.get("token")}`,
+        },
+      }
+    );
+    const appointmentData = await appointments.json();
+    setTotalCountData((prev) => [
+      ...prev,
+      {
+        title: "Total Appointments",
+        count: appointmentData.length,
+        icon: "/calendar-ad.png",
+        bg: "bg-gradient-purple-card",
+      },
+    ]);
+  };
+
+  useEffect(() => {
+    fetchUsers();
+    fetchAppointments();
+  }, []);
 
   return (
     <div className="w-full grid lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1 gap-5">
