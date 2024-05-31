a1303e755b884d57726dcb10ad5343b6906ff9a2
joshuabriones <joshua.briones@cit.edu>
2024-05-31 23:23:02 +0800

    emergency btn - cursor pointer

--- components/ui/emergency/FloatingIcon.jsx
+++ components/ui/emergency/FloatingIcon.jsx
@@ -5,20 +5,20 @@ import { useState } from "react";
 import EmergencyModal from "./Emergency";
 
 const FloatingIcon = () => {
-	const [openEmergencyModal, setOpenEmergencyModal] = useState(false);
+  const [openEmergencyModal, setOpenEmergencyModal] = useState(false);
 
-	return (
-		<>
-			<div
-				className="floating-icon hover:scale-125"
-				onClick={() => setOpenEmergencyModal(true)}
-			>
-				<img src={phone.src} alt="terms&condition" className="w-12 h-12" />
-			</div>
+  return (
+    <>
+      <div
+        className="floating-icon hover:scale-125 duration-200 cursor-pointer animate-pulse"
+        onClick={() => setOpenEmergencyModal(true)}
+      >
+        <img src={phone.src} alt="terms&condition" className="w-12 h-12" />
+      </div>
 
-			{openEmergencyModal && <EmergencyModal onOpen={setOpenEmergencyModal} />}
-		</>
-	);
+      {openEmergencyModal && <EmergencyModal onOpen={setOpenEmergencyModal} />}
+    </>
+  );
 };
 
 export default FloatingIcon;
