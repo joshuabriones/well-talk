1a3bf1019aee4afc68489ad30d94ac45153a7c33
Gieb Mier <128208941+giebm@users.noreply.github.com>
2024-09-26 20:29:36 +0800

    referral confirmation

--- app/referral/[token]/pendingreferral/page.jsx
+++ app/referral/[token]/pendingreferral/page.jsx
@@ -1,67 +1,93 @@
 "use client";
 
 import { API_ENDPOINT } from "@/lib/api";
-import { useState, useEffect } from "react";
-import { useSearchParams, usePathname } from "next/navigation";
+import { usePathname, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
 
 import NotFoundPage from "@/app/not-found";
+import FullButton from "@/components/ui/buttons/FullButton";
+import HollowButton from "@/components/ui/buttons/HollowButton";
 
 const PendingReferral = () => {
-  const searchParams = useSearchParams();
-  const pathname = usePathname();
-  const [isTokenValid, setIsTokenValid] = useState(false);
-  const [isLoading, setIsLoading] = useState(true);
-  const [token, setToken] = useState("");
+	const searchParams = useSearchParams();
+	const pathname = usePathname();
+	const [isTokenValid, setIsTokenValid] = useState(false);
+	const [isLoading, setIsLoading] = useState(true);
+	const [token, setToken] = useState("");
 
-  useEffect(() => {
-    if (pathname && searchParams) {
-      const tokenFromPath = pathname.split("/")[2];
-      console.log(tokenFromPath);
-      handleTokenValidation(tokenFromPath);
-    }
-  }, [pathname, searchParams]);
+	const [confirm, setConfirm] = useState(false);
 
-  const handleTokenValidation = async (tokenFromPath) => {
-    try {
-      const response = await fetch(
-        `${process.env.BASE_URL}${API_ENDPOINT.VALIDATE_REFERRAL_TOKEN}${tokenFromPath}`,
-        {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
+	console.log(confirm);
 
-      if (response.status === 200) {
-        setIsTokenValid(true);
-      } else {
-        setIsTokenValid(false);
-      }
-    } catch (error) {
-      console.error(error);
-    } finally {
-      setIsLoading(false);
-    }
-  };
+	useEffect(() => {
+		if (pathname && searchParams) {
+			const tokenFromPath = pathname.split("/")[2];
+			console.log(tokenFromPath);
+			handleTokenValidation(tokenFromPath);
+		}
+	}, [pathname, searchParams]);
 
-  if (isLoading) {
-    return (
-      <div className="min-h-screen flex items-center justify-center">
-        <p>Loading...</p>
-      </div>
-    );
-  }
+	const handleTokenValidation = async (tokenFromPath) => {
+		try {
+			const response = await fetch(
+				`${process.env.BASE_URL}${API_ENDPOINT.VALIDATE_REFERRAL_TOKEN}${tokenFromPath}`,
+				{
+					method: "GET",
+					headers: {
+						"Content-Type": "application/json",
+					},
+				}
+			);
 
-  if (isTokenValid) {
-    return (
-      <div>
-        <h1>Referral</h1>
-      </div>
-    );
-  } else {
-    return <NotFoundPage />;
-  }
+			if (response.status === 200) {
+				setIsTokenValid(true);
+			} else {
+				setIsTokenValid(false);
+			}
+		} catch (error) {
+			console.error(error);
+		} finally {
+			setIsLoading(false);
+		}
+	};
+
+	if (isLoading) {
+		return (
+			<div className="min-h-screen flex items-center justify-center">
+				<p>Loading...</p>
+			</div>
+		);
+	}
+
+	if (isTokenValid) {
+		return (
+			<div className="h-screen w-full flex justify-center items-center">
+				<div className="w-[600px] h-[400px] rounded-xl py-2 px-10 flex flex-col justify-center items-center gap-y-3 shadow-xl">
+					<div className="">
+						<img src="/images/loggo.png" alt="" className="h-28" />
+					</div>
+
+					<div className="font-Merriweather text-center">
+						Please confirm if you'd like to proceed with your referral to the guidance
+						office. We appreciate your time and assistance.
+					</div>
+
+					<div className="w-10/12 flex gap-3 mt-5 mx-auto">
+						<HollowButton>Decline</HollowButton>
+						<FullButton
+							onClick={() => {
+								setConfirm(true);
+							}}
+						>
+							Proceed
+						</FullButton>
+					</div>
+				</div>
+			</div>
+		);
+	} else {
+		return <NotFoundPage />;
+	}
 };
 
 export default PendingReferral;
--- components/ui/Notifications.jsx
+++ components/ui/Notifications.jsx
@@ -1,40 +1,88 @@
-import { getUserSession } from "@/lib/helperFunctions";
-import { useEffect, useState } from "react";
+const user = {
+	id: 99,
+	name: "France Gieb S. Mier",
+	img: "https://avatar.iran.liara.run/public",
+};
 
 const notifs = [
 	{
 		id: 1,
-		rptId: 99,
-		rptName: "France Gieb S. Mier",
-		rptImg: "https://avatar.iran.liara.run/public",
+		receiver: {
+			id: 10,
+			name: "Bomba Ding",
+			img: "https://avatar.iran.liara.run/public",
+		},
+		sender: {
+			id: 99,
+			name: "France Gieb S. Mier",
+			img: "https://avatar.iran.liara.run/public",
+		},
+		// title,
 		type: "Appointment",
+		isRead: "false",
 		timestamp: "2024-08-12T12:45:00.000Z",
+
+		appointment: {
+			id: 20,
+			date: "2024-08-12",
+		},
 	},
 ];
 
 export default function Notifications() {
-	const userSession = getUserSession();
+	const generateNotif = (notif) => {
+		let text = "";
 
-	const [user, setUser] = useState(null);
+		switch (notif.type) {
+			case "Appointment":
+				if (user.id === notif.sender.id) {
+					text = `You have set an appointment with Counselor ${notif.receiver.name} on ${notif.appointment.date}.`;
+				}
+				break;
+		}
 
-	useEffect(() => {
-		setUser(userSession);
-	}, []);
-
-	console.log(user);
+		return text;
+	};
 
 	return (
-		<div className="bg-white absolute z-10 w-1/4 h-[600px] top-14 right-44 rounded-lg drop-shadow-2xl flex flex-col ">
+		<div className="bg-white absolute z-10 w-[500px] h-[600px] top-14 right-44 rounded-lg drop-shadow-2xl flex flex-col ">
 			<div className="flex justify-between text-sm border-b border-slate-200 p-5">
 				<div>Bomba Ding's Notifications</div>
 				<div className="text-blue-600">Mark All as Read</div>
 			</div>
 
 			{/* Notification List */}
-			<div className="bg-amber-400 h-32 w-full flex flex-row">
-				<div className="bg-pink-400 h-full w-1/5"></div>
-				<div className="bg-pink-700 h-full flex-grow"></div>
-				<div className="bg-pink-400 h-full w-1/12"></div>
+			<div className="flex flex-grow flex-col">
+				<div className="h-24 w-full flex flex-row px-2 group hover:bg-zinc-100">
+					<div className="h-full w-2/12 flex justify-center items-center">
+						<img
+							src="https://avatar.iran.liara.run/public"
+							alt=""
+							className="rounded-full h-12 w-12"
+						/>
+					</div>
+
+					<div className="h-full w-9/12 pl-1.5 flex items-center justify-center text-sm">
+						{generateNotif(notifs[0])}
+					</div>
+
+					<div className="h-full w-1/12 flex items-center invisible group-hover:visible">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							strokeWidth={1.5}
+							stroke="black"
+							className="size-6  rounded-full p-1 hover:bg-zinc-200"
+						>
+							<path
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								d="M6.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM12.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM18.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
+							/>
+						</svg>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
