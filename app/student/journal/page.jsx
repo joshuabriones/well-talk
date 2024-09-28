ad3ec97e3dcd56005fb8691b1d8cc7743d79c5ca
showah <146901959+showah@users.noreply.github.com>
2024-09-28 18:21:11 +0800

    journal mobile responsive

--- app/student/journal/page.jsx
+++ app/student/journal/page.jsx
@@ -1,6 +1,7 @@
 "use client";
 import JournalList from "@/components/JournalList";
 import { Navbar } from "@/components/ui/Navbar";
+import Toggle from "@/components/ui/toggle/Toggle";
 import { API_ENDPOINT } from "@/lib/api";
 import { getUserSession } from "@/lib/helperFunctions";
 import "@/styles/globals.css";
@@ -8,13 +9,11 @@ import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
 import Cookies from "js-cookie";
 import dynamic from "next/dynamic";
-import Image from "next/image";
 import { useEffect, useState } from "react";
 import { toast } from "react-hot-toast";
 // import ReactHtmlParser from 'react-html-parser';
 import parse from "html-react-parser";
 // import PublicizedToggle from "@/components/ui/toggle/PublicizedToggle";
-import Toggle from "@/components/ui/toggle/Toggle";
 // import "@/css/toggle.css";
 import JournalModal from "./_modal/JournalModal";
 
@@ -28,6 +27,12 @@ const StudentJournal = () => {
 	const [highlightEntry, setHighlightEntry] = useState({});
 	const [isEditing, setIsEditing] = useState(false);
 	const [showModal, setShowModal] = useState(false);
+	const [showEntry, setShowEntry] = useState(false); // New state for showing entry
+
+	const handleEntryClick = () => {
+		// This function is called when a journal entry is clicked
+		setShowEntry(true);
+	};
 
 	// if (Cookies.get("token") === undefined || Cookies.get("token") === null) {
 	// 	return <Load route="login" />;
@@ -64,10 +69,13 @@ const StudentJournal = () => {
 
 	const handleClickedEntry = (journalId) => {
 		if (!isEditing) {
-			const selectedEntry = journalEntries.find((entry) => entry.journalId === journalId);
+			const selectedEntry = journalEntries.find(
+				(entry) => entry.journalId === journalId
+			);
 
 			if (selectedEntry) {
 				setHighlightEntry(selectedEntry);
+				setShowEntry(true);
 			}
 		}
 	};
@@ -125,17 +133,24 @@ const StudentJournal = () => {
 			);
 
 			if (response.ok) {
-				const deletedEntry = journalEntries.find((entry) => entry.journalId === journalId);
+				const deletedEntry = journalEntries.find(
+					(entry) => entry.journalId === journalId
+				);
 				toast.success(
 					`Journal entry with title "${deletedEntry.title}" deleted successfully.`
 				);
 				fetchEntries();
 				setIsEditing(false);
 			} else {
-				toast.error(`Failed to delete journal entry with title "${deletedEntry.title}".`);
+				toast.error(
+					`Failed to delete journal entry with title "${deletedEntry.title}".`
+				);
 			}
 		} catch (error) {
-			console.error("Error occurred while deleting journal entry:", error);
+			console.error(
+				"Error occurred while deleting journal entry:",
+				error
+			);
 		}
 	};
 
@@ -163,7 +178,9 @@ const StudentJournal = () => {
 				// Update the state with the updated entry
 				setJournalEntries(
 					journalEntries.map((entry) =>
-						entry.journalId === updatedEntry.journalId ? updatedEntry : entry
+						entry.journalId === updatedEntry.journalId
+							? updatedEntry
+							: entry
 					)
 				);
 				setHighlightEntry(updatedEntry);
@@ -212,9 +229,31 @@ const StudentJournal = () => {
 		<div className="min-h-screen">
 			<Navbar userType="student" />
 			<div className="h-screen w-full font-Merriweather">
-				<div className="w-full h-full pt-24 pb-14 px-32 flex lg:flex-row xs:flex-col">
+				<div className="w-full h-full pt-24 pb-14 px-4 md:px-8 lg:px-32 flex flex-col lg:flex-row">
 					{/* List */}
-					<section className="w-1/3 h-full py-3 overflow-y-auto">
+					<section className="w-full lg:w-1/3 h-full py-3 overflow-y-auto">
+						<button
+							className="z-10 tooltip tooltip-success w-full md:w-11/12 lg:w-11/12 mb-4 py-2 rounded-2xl bg-maroon flex items-center justify-center gap-2"
+							onClick={() => setShowModal(true)} // Assuming setShowModal exists
+						>
+							{/* Add Icon */}
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 24 24"
+								strokeWidth={2}
+								stroke="#FFFFFF"
+								className="w-7 h-6">
+								<path
+									strokeLinecap="round"
+									strokeLinejoin="round"
+									d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m3.75 9v6m3-3H9m1.5-12H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z"
+								/>
+							</svg>
+							{/* Add Text */}
+							<span className="text-white">Add Entry</span>
+						</button>
+
 						<JournalList
 							entries={journalEntries}
 							isEditing={isEditing}
@@ -224,216 +263,193 @@ const StudentJournal = () => {
 					</section>
 
 					{/* Journal Highlight */}
-					<section className="w-2/3 h-full pt-14 pb-16 px-16 rounded-2xl border-2 shadow-2xl relative md:w-3/4 xs:w-full">
-						{/* Buttons */}
-						<div className="flex gap-3 items-center justify-end">
-							{isEditing ? (
-								<>
-									<button
-										className="z-10 tooltip tooltip-success text-maroon"
-										data-tip={`${isEditing ? "Cancel" : "Edit"}`}
-										onClick={() => setIsEditing((prevState) => !prevState)}
-									>
-										{/* <Image
-											src={"/images/icons/edit.png"}
-											width={25}
-											height={25}
-											alt="Edit Icon"
-										/> */}
-										<svg
-											xmlns="http://www.w3.org/2000/svg"
-											fill="none"
-											viewBox="0 0 24 24"
-											strokeWidth={2}
-											stroke="#8A252C"
-											className="w-7 h-7"
-										>
-											<path
-												strokeLinecap="round"
-												strokeLinejoin="round"
-												d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
-											/>
-										</svg>
-									</button>
-
-									<button
-										className="z-10 tooltip tooltip-success"
-										data-tip="Save Changes"
-										onClick={handleEditEntry}
-									>
-										{/* <Image
-											src={"/images/icons/saveEdit.png"}
-											width={25}
-											height={25}
-											alt="Save Changes Icon"
-										/> */}
-										<svg
-											xmlns="http://www.w3.org/2000/svg"
-											fill="none"
-											viewBox="0 0 24 24"
-											strokeWidth={2}
-											stroke="#8A252C"
-											className="w-7 h-7"
-										>
-											<path
-												strokeLinecap="round"
-												strokeLinejoin="round"
-												d="M16.5 3.75V16.5L12 14.25 7.5 16.5V3.75m9 0H18A2.25 2.25 0 0 1 20.25 6v12A2.25 2.25 0 0 1 18 20.25H6A2.25 2.25 0 0 1 3.75 18V6A2.25 2.25 0 0 1 6 3.75h1.5m9 0h-9"
-											/>
-										</svg>
-									</button>
-								</>
-							) : (
-								<>
-									<button
-										className="z-10 tooltip tooltip-success"
-										data-tip="Edit"
-										onClick={() => setIsEditing((prevState) => !prevState)}
-										disabled={journalEntries.length === 0}
-									>
-										<svg
-											xmlns="http://www.w3.org/2000/svg"
-											fill="none"
-											viewBox="0 0 24 24"
-											strokeWidth={2}
-											stroke="#8A252C"
-											className="w-7 h-7"
-										>
-											<path
-												strokeLinecap="round"
-												strokeLinejoin="round"
-												d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
-											/>
-										</svg>
-									</button>
-
-									<button
-										className="z-10 tooltip tooltip-success"
-										data-tip="New Entry"
-										onClick={() => setShowModal(true)}
-									>
-										{/* <Image
-											src={"/images/icons/addjournal.png"}
-											width={25}
-											height={25}
-											alt="Add Journal Icon"
-										/> */}
-										<svg
-											xmlns="http://www.w3.org/2000/svg"
-											fill="none"
-											viewBox="0 0 24 24"
-											strokeWidth={2}
-											stroke="#8A252C"
-											className="w-7 h-6"
-										>
-											<path
-												strokeLinecap="round"
-												strokeLinejoin="round"
-												d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m3.75 9v6m3-3H9m1.5-12H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z"
-											/>
-										</svg>
-									</button>
-								</>
-							)}
-						</div>
-
-						<div className="w-full h-full flex flex-col overflow-y-auto p-5">
-							{/* Title Section */}
-							<div className="flex-none mb-5">
-								{isEditing ? (
-									<input
-										type="text"
-										className="text-4xl text-maroon w-full bg-white border"
-										value={editTitle}
-										onChange={(e) => setEditTitle(e.target.value)}
-									/>
-								) : (
-									<h1 className="text-5xl font-semibold text-maroon">
-										{highlightEntry?.title}
-									</h1>
-								)}
-							</div>
-
-							{/* Entry Section */}
-							<div className="flex-grow mb-5">
-								{isEditing ? (
-									<div className="ckeditor-custom">
-										<CKEditor
-											editor={ClassicEditor}
-											data={editEntry}
-											onChange={(event, editor) => {
-												const data = editor.getData();
-												setEditEntry(data);
-											}}
-										/>
-									</div>
-								) : (
-									<div className="text-lg font-light text-gray">
-										{parse(highlightEntry?.entry || "")}
-									</div>
-								)}
-							</div>
-
-							{/* Dates Section */}
-							<div className="flex flex-row items-end justify-between">
-								<div className="flex-none mt-auto font-Jaldi text-s italic text-gray-600">
-									{highlightEntry === null ? null : (
-										<div>
-											Date of Entry:{" "}
-											{`${dateFormatter(
-												highlightEntry?.dateOfEntry
-											)}, ${timeFormatter(highlightEntry?.timeOfEntry)}`}
-										</div>
-									)}
-
-									{highlightEntry?.dateOfUpdate !== "January 1, 1970" &&
-									highlightEntry?.timeOfUpdate ? (
-										<div>
-											Last Updated:{" "}
-											{`${dateFormatter(
-												highlightEntry?.dateOfUpdate
-											)}, ${timeFormatter(highlightEntry?.timeOfUpdate)}`}
-										</div>
-									) : null}
-								</div>
-								<div className="w-fit flex flex-col items-end justify-end gap-y-1.5">
+					{showEntry && ( // Render the section only if showEntry is true
+						<section className="w-full lg:w-2/3 h-full md:h-full lg:h-full pb-20 px-4 md:px-8 lg:px-16 rounded-2xl border-2 shadow-2xl relative">
+							{/* Buttons */}
+							<div className="flex justify-between items-center mb-4">
+								{/* Toggle placed on the left side */}
+								<div className="w-fit flex flex-col items-start justify-start gap-y-1.5">
 									<Toggle />
 									<div className="font-Jaldi text-s italic text-gray-600">
 										Toggle to provide access for counselor.
 									</div>
 								</div>
+
+								{/* Button container on the right side */}
+								<div className="flex gap-3 items-center">
+									{isEditing ? (
+										<>
+											<button
+												className="z-10 tooltip tooltip-success text-maroon"
+												data-tip={`${
+													isEditing
+														? "Cancel"
+														: "Edit"
+												}`}
+												onClick={() =>
+													setIsEditing(
+														(prevState) =>
+															!prevState
+													)
+												}>
+												{/* Edit Icon */}
+												<svg
+													xmlns="http://www.w3.org/2000/svg"
+													fill="none"
+													viewBox="0 0 24 24"
+													strokeWidth={2}
+													stroke="#8A252C"
+													className="w-7 h-7">
+													<path
+														strokeLinecap="round"
+														strokeLinejoin="round"
+														d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
+													/>
+												</svg>
+											</button>
+
+											<button
+												className="z-10 tooltip tooltip-success"
+												data-tip="Save Changes"
+												onClick={handleEditEntry}>
+												{/* Save Icon */}
+												<svg
+													xmlns="http://www.w3.org/2000/svg"
+													fill="none"
+													viewBox="0 0 24 24"
+													strokeWidth={2}
+													stroke="#8A252C"
+													className="w-7 h-7">
+													<path
+														strokeLinecap="round"
+														strokeLinejoin="round"
+														d="M16.5 3.75V16.5L12 14.25 7.5 16.5V3.75m9 0H18A2.25 2.25 0 0 1 20.25 6v12A2.25 2.25 0 0 1 18 20.25H6A2.25 2.25 0 0 1 3.75 18V6A2.25 2.25 0 0 1 6 3.75h1.5m9 0h-9"
+													/>
+												</svg>
+											</button>
+										</>
+									) : (
+										<>
+											<button
+												className="z-10 tooltip tooltip-success"
+												data-tip="Edit"
+												onClick={() =>
+													setIsEditing(
+														(prevState) =>
+															!prevState
+													)
+												}
+												disabled={
+													journalEntries.length === 0
+												}>
+												{/* Edit Icon */}
+												<svg
+													xmlns="http://www.w3.org/2000/svg"
+													fill="none"
+													viewBox="0 0 24 24"
+													strokeWidth={2}
+													stroke="#8A252C"
+													className="w-7 h-7">
+													<path
+														strokeLinecap="round"
+														strokeLinejoin="round"
+														d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
+													/>
+												</svg>
+											</button>
+											<button
+												className="z-10 tooltip tooltip-error"
+												data-tip="Close"
+												onClick={() =>
+													setShowEntry(null)
+												}>
+												<svg
+													xmlns="http://www.w3.org/2000/svg"
+													fill="none"
+													viewBox="0 0 24 24"
+													strokeWidth={2}
+													stroke="#8A252C"
+													className="w-7 h-7">
+													<path
+														strokeLinecap="round"
+														strokeLinejoin="round"
+														d="M6 18L18 6M6 6l12 12"
+													/>
+												</svg>
+											</button>
+										</>
+									)}
+								</div>
 							</div>
 
-							{/* Images */}
-							<Image
-								src={"/images/journal-spring.png"}
-								height={48}
-								width={22}
-								className="absolute -left-2 top-12"
-								alt="Journal Spring"
-							/>
+							<div className="w-full h-full flex flex-col overflow-y-auto p-5">
+								{/* Title Section */}
+								<div className="flex-none mb-5">
+									{isEditing ? (
+										<input
+											type="text"
+											className="text-4xl text-maroon w-full bg-white border"
+											value={editTitle}
+											onChange={(e) =>
+												setEditTitle(e.target.value)
+											}
+										/>
+									) : (
+										<h1 className="text-5xl font-semibold text-maroon">
+											{highlightEntry?.title}
+										</h1>
+									)}
+								</div>
 
-							<Image
-								src={"/images/journal-spring.png"}
-								height={48}
-								width={22}
-								className="absolute -left-2 bottom-12"
-								alt="Journal Spring"
-							/>
-						</div>
-					</section>
+								{/* Entry Section */}
+								<div className="flex-grow mb-5">
+									{isEditing ? (
+										<div className="ckeditor-custom">
+											<CKEditor
+												editor={ClassicEditor}
+												data={editEntry}
+												onChange={(event, editor) => {
+													const data =
+														editor.getData();
+													setEditEntry(data);
+												}}
+											/>
+										</div>
+									) : (
+										<div className="text-lg font-light text-gray">
+											{parse(highlightEntry?.entry || "")}
+										</div>
+									)}
+								</div>
 
-					{showModal && (
-						<JournalModal
-							title={title}
-							entry={entry}
-							setTitle={setTitle}
-							setEntry={setEntry}
-							setShowModal={setShowModal}
-							handleSaveEntry={handleSaveEntry}
-						/>
+								{/* Dates Section */}
+								<div className="flex-none mb-8 font-Jaldi text-s italic text-gray-600">
+									{highlightEntry === null ? null : (
+										<div>
+											Date of Entry:{" "}
+											{`${dateFormatter(
+												highlightEntry?.dateOfEntry
+											)}, ${timeFormatter(
+												highlightEntry?.timeOfEntry
+											)}`}
+										</div>
+									)}
+								</div>
+							</div>
+						</section>
 					)}
 				</div>
+
+				{showModal && (
+					<JournalModal
+						title={title}
+						entry={entry}
+						setTitle={setTitle}
+						setEntry={setEntry}
+						setShowModal={setShowModal}
+						handleSaveEntry={handleSaveEntry}
+					/>
+				)}
 			</div>
 		</div>
 	);
