79d3d1bcf8dd4ae5aebe56bbb1ebf59609aaa7fc
keisamae <kheisa.selma@cit.edu>
2024-10-02 18:54:47 +0800

    referral

--- app/referral/[token]/pendingreferral/page.jsx
+++ app/referral/[token]/pendingreferral/page.jsx
@@ -5,96 +5,117 @@ import FullButton from "@/components/ui/buttons/FullButton";
 import { API_ENDPOINT } from "@/lib/api";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
+import ConfirmationPopup from "@/components/ui/modals/Confirmation";
 
 const PendingReferral = () => {
-	const router = useRouter();
+  const router = useRouter();
 
-	const searchParams = useSearchParams();
-	const pathname = usePathname();
-	const [isTokenValid, setIsTokenValid] = useState(false);
-	const [isLoading, setIsLoading] = useState(true);
-	const [token, setToken] = useState("");
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const [isTokenValid, setIsTokenValid] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [token, setToken] = useState("");
+  const [showConfirm, setShowConfirm] = useState(false);
+  const [title, setTitle] = useState();
+  const [message, setMessage] = useState();
 
-	console.log(confirm);
+  useEffect(() => {
+    if (pathname && searchParams) {
+      const tokenFromPath = pathname.split("/")[2];
+      console.log(tokenFromPath);
+      handleTokenValidation(tokenFromPath);
+    }
+  }, [pathname, searchParams]);
 
-	useEffect(() => {
-		if (pathname && searchParams) {
-			const tokenFromPath = pathname.split("/")[2];
-			console.log(tokenFromPath);
-			handleTokenValidation(tokenFromPath);
-		}
-	}, [pathname, searchParams]);
+  const handleTokenValidation = async (tokenFromPath) => {
+    try {
+      const response = await fetch(
+        `${process.env.BASE_URL}${API_ENDPOINT.VALIDATE_REFERRAL_TOKEN}${tokenFromPath}`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
 
-	const handleTokenValidation = async (tokenFromPath) => {
-		try {
-			const response = await fetch(
-				`${process.env.BASE_URL}${API_ENDPOINT.VALIDATE_REFERRAL_TOKEN}${tokenFromPath}`,
-				{
-					method: "GET",
-					headers: {
-						"Content-Type": "application/json",
-					},
-				}
-			);
+      if (response.status === 200) {
+        setIsTokenValid(true);
+      } else {
+        setIsTokenValid(false);
+      }
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
-			if (response.status === 200) {
-				setIsTokenValid(true);
-			} else {
-				setIsTokenValid(false);
-			}
-		} catch (error) {
-			console.error(error);
-		} finally {
-			setIsLoading(false);
-		}
-	};
+  if (isLoading) {
+    return (
+      <div className="min-h-screen flex items-center justify-center">
+        <p>Loading...</p>
+      </div>
+    );
+  }
 
-	if (isLoading) {
-		return (
-			<div className="min-h-screen flex items-center justify-center">
-				<p>Loading...</p>
-			</div>
-		);
-	}
+  const handleConfirm = async () => {
+    setTitle("Accept Referral");
+    setMessage("Are you sure you want to accept referral?");
+    setShowConfirm(true);
+  };
 
-	const handleConfirm = async () => {
-		console.log("Confirm referral");
-		router.push("/login");
-	};
+  const handleDecline = async () => {
+    setTitle("Decline Referral");
+    setMessage("Are you sure you want to decline referral?");
+    setShowConfirm(true);
+  };
 
-	const handleDecline = async () => {
-		console.log("Decline referral");
-		router.push("/login");
-	};
+  //   const acceptReferral = async () = {
+  // 	try {
 
-	if (isTokenValid) {
-		return (
-			<div className="h-screen w-full flex justify-center items-center">
-				<div className="w-[600px] h-[400px] rounded-xl py-2 px-10 flex flex-col justify-center items-center gap-y-2 shadow-xl">
-					<div className="">
-						<img src="/images/loggo.png" alt="" className="h-28" />
-					</div>
+  // 	} catch (error) {
 
-					<div className="font-Merriweather text-center">
-						Please confirm if you'd like to proceed with your referral to the guidance
-						office. We appreciate your time and assistance.
-					</div>
+  // 	}
+  //   }
 
-					<div className="w-10/12 flex flex-col items-center mt-4 gap-1.5">
-						<FullButton onClick={handleConfirm}>I would like to proceed.</FullButton>
-						<div
-							className="text-xs cursor-pointer hover:scale-95 hover:text-[#8a252c]"
-							onClick={handleDecline}
-						>
-							Decline
-						</div>
-					</div>
-				</div>
-			</div>
-		);
-	} else {
-		return <NotFoundPage />;
-	}
+  if (isTokenValid) {
+    return (
+      <div className="h-screen w-full flex justify-center items-center">
+        <div className="w-[600px] h-[400px] rounded-xl py-2 px-10 flex flex-col justify-center items-center gap-y-2 shadow-xl">
+          <div className="">
+            <img src="/images/loggo.png" alt="" className="h-28" />
+          </div>
+
+          <div className="font-Merriweather text-center">
+            Please confirm if you'd like to proceed with your referral to the
+            guidance office. We appreciate your time and assistance.
+          </div>
+
+          <div className="w-10/12 flex flex-col items-center mt-4 gap-1.5">
+            <FullButton onClick={handleConfirm}>
+              I would like to proceed.
+            </FullButton>
+            <div
+              className="text-xs cursor-pointer hover:scale-95 hover:text-[#8a252c]"
+              onClick={handleDecline}
+            >
+              Decline
+            </div>
+          </div>
+        </div>
+        {showConfirm && (
+          <ConfirmationPopup
+            title={title}
+            message={message}
+            onCancel={() => setShowConfirm(false)}
+          />
+        )}
+      </div>
+    );
+  } else {
+    return <NotFoundPage />;
+  }
 };
 
 export default PendingReferral;
