1fab4e86c7f0bfe52bd0768893ebf27e4cce1277
joshuabriones <joshua.briones@cit.edu>
2024-10-03 16:28:45 +0800

    fixing deployment errors

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,29 +2,26 @@ import Provider from "@/components/Provider";
 import ChatByRole from "@/components/ui/chat/ChatByRole";
 import FloatingIcon from "@/components/ui/emergency/FloatingIcon";
 import "@/styles/globals.css";
-import { getServerSession } from "next-auth";
 import { Inter } from "next/font/google";
 import { Toaster } from "react-hot-toast";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-	title: "Well Talk: Communicate",
-	description: "This is a pogi app na gawa ng mga imba 😎",
+  title: "Well Talk: Communicate",
+  description: "This is a pogi app na gawa ng mga imba 😎",
 };
 
 export default async function RootLayout({ children }) {
-	const userSession = await getServerSession();
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Toaster position="top-center" />
+        <FloatingIcon />
 
-	return (
-		<html lang="en">
-			<body className={inter.className}>
-				<Toaster position="top-center" />
-				<FloatingIcon />
-
-				<ChatByRole />
-				<Provider>{children}</Provider>
-			</body>
-		</html>
-	);
+        <ChatByRole />
+        <Provider>{children}</Provider>
+      </body>
+    </html>
+  );
 }
