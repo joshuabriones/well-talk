0c96fc23ff8dab79c79f5a51af755b21d9cad84f
showah <146901959+showah@users.noreply.github.com>
2024-10-06 13:22:46 +0800

    adjusted notif button on mobile

--- components/ui/Navbar.jsx
+++ components/ui/Navbar.jsx
@@ -88,7 +88,7 @@ function ProfileMenu() {
     <div>
       <button
         onClick={() => setIsMenuOpen((prev) => !prev)}
-        className="flex items-center gap-2 rounded-full py-3 pr-2 mr-6  md:mr-20 pl-0.5 lg:ml-auto"
+        className="flex items-center gap-2 rounded-full py-3  mr-4  md:mr-20 pl-0.5 lg:ml-auto"
       >
         <img
           src={userData?.image}
@@ -297,7 +297,7 @@ export function Navbar({ userType }) {
         >
           <motion.img
             src="/images/bellll.png"
-            className="w-9 h-9 md:w-9 md:h-9 rounded-2xl cursor-pointer"
+            className="w-9 h-9 md:w-9 md:h-9 rounded-2xl cursor-pointer mx-4"
             alt="notiffs"
             initial={{ scale: 1 }}
             whileHover={{ scale: 1.1 }}
