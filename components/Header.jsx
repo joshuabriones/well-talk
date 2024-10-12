9a8ae6dbaf5a530f7421770e43420b9a0635d80f
showah <146901959+showah@users.noreply.github.com>
2024-10-12 15:37:52 +0800

    added referral in counselor navlist

--- components/Header.jsx
+++ components/Header.jsx
@@ -10,7 +10,7 @@ export default function Header(props) {
 			></div>
 
 			{/* Content */}
-			<div className="relative z-10 flex items-center justify-center h-full">
+			<div className="relative z-10 flex justify-start items-center h-full">
 				<div className="flex flex-col text-left px-44 py-10 gap-y-4">
 					<h1 className="font-Merriweather text-8xl">Referrals</h1>
 					<p className="w-1/2 font-Jaldi text-xl">{props.desc}</p>
