036bee83b8cd0c03f1f0491502a6611a4dab6dfa
showah <146901959+showah@users.noreply.github.com>
2024-10-12 15:37:30 +0800

    register success modal adjustments

--- components/ui/modals/ModalRegistrationSuccessful.jsx
+++ components/ui/modals/ModalRegistrationSuccessful.jsx
@@ -1,45 +1,35 @@
-import registeredUser from "@/public/images/registeredUser.png";
+import registeredUser from "@/public/images/registered.png";
 import FullButton from "../buttons/FullButton";
 
 const ModalRegistrationSuccessful = ({
 	setShowRegistrationSuccessful,
-	registrationDetails, // to be deleted
 	handleLoginClick,
 }) => {
 	return (
-		<>
-			<input
-				type="checkbox"
-				id="my_modal_8"
-				className="modal-toggle"
-				checked={true}
-			/>
-			<div className="modal" role="dialog">
-				<div className="modal-box p-9 text-center">
-					<img
-						src={registeredUser.src}
-						alt="terms&condition"
-						className="w-24 h-24 flex justify-center mx-auto"
-					/>
-					<h3 className="text-xl font-bold py-2">
-						Registration Successful
-					</h3>
-					<p className="text-sm py-4">
-						Registration has been successful. You may now proceed to login page! {/* to be deleted */}
-					</p>
-					<FullButton onClick={handleLoginClick}>
-						Go to Login
-					</FullButton>
+		<div
+			className="fixed inset-0 flex items-center justify-center bg-white bg-opacity-25 z-50 backdrop-blur"
+			role="dialog"
+		>
+				<div className="modal-box relative p-4 sm:p-6 lg:p-9 border-2 text-center max-w-xs sm:max-w-lg bg-white rounded-2xl shadow-lg transform transition-transform duration-300 scale-95">
+				<img
+					src={registeredUser.src}
+					alt="Registration Successful"
+					className="w-20 h-20 sm:w-28 sm:h-28 mx-auto"
+				/>
+				<h3 className="text-lg sm:text-xl font-bold font-Merriweather py-4 sm:py-6">
+					Registration Successful
+				</h3>
+				<p className="text-sm sm:text-base text-gray-600">
+					Registration has been successful. Your account has been sent for approval. Please wait for the admin to verify your account before you can access it.
+				</p>
+
+				<div className="flex flex-col sm:flex-row justify-center gap-y-4 sm:gap-x-4 py-4 sm:py-6 px-6 sm:px-12">
+				<FullButton onClick={handleLoginClick}>
+					Back to Login
+				</FullButton>
 				</div>
-				<label
-					className="modal-backdrop"
-					htmlFor="my_modal_8"
-					onClick={() => setShowRegistrationSuccessful(false)}
-				>
-					Close
-				</label>
 			</div>
-		</>
+		</div>
 	);
 };
 
