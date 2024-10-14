da2aea023b2b7df231d26dabb290c38b7e031548
Gieb Mier <128208941+giebm@users.noreply.github.com>
2024-10-15 02:35:05 +0800

    notif route app modals

--- components/ui/Notifications.jsx
+++ components/ui/Notifications.jsx
@@ -8,6 +8,9 @@ import Cookies from "js-cookie";
 import { useRouter } from "next/navigation";
 import { useContext, useEffect, useState } from "react";
 
+import AppointmentHighlight from "@/components/ui/modals/notifications/AppointmentHighlight";
+import AppointmentDone from "./modals/notifications/AppointmentDone";
+
 export default function Notifications() {
 	const router = useRouter();
 	const userSession = getUserSession();
@@ -17,6 +20,10 @@ export default function Notifications() {
 	const [user, setUser] = useState(null);
 	const [notifications, setNotifications] = useState([]);
 	const [notificationToDelete, setNotificationToDelete] = useState(null);
+
+	const [appointment, setAppointment] = useState(null);
+	const [appointmentModal, setAppointmentModal] = useState(false);
+	const [appointmentDoneModal, setAppointmentDoneModal] = useState(false);
 
 	console.log("notifications:", notifications);
 	// console.log("User Session Id: ", userSession.id);
@@ -208,13 +215,12 @@ export default function Notifications() {
 
 		switch (notification?.type) {
 			case "appointment":
-				if (user?.role === "student") {
-					router.push(`/student/appointment`);
-				}
-				if (user?.role === "counselor") {
-					router.push(`/counselor/counselor-appointment`);
-				}
+				setAppointment(notification?.appointment);
+				setAppointmentModal(true);
 				break;
+			case "appointment_done":
+				setAppointment(notification?.appointment);
+				setAppointmentDoneModal(true);
 
 			case "referral":
 				if (user?.role === "student") {
@@ -307,7 +313,7 @@ export default function Notifications() {
 					}
 					break;
 				case "appointment_done":
-					text = `Your appointment with Counselor ${senderName} has been successfully completed!`;
+					text = `Your appointment with Counselor ${senderName} is marked done.`;
 					break;
 
 				case "referral":
@@ -470,115 +476,123 @@ export default function Notifications() {
 	};
 
 	return (
-		<div className="bg-white absolute z-10 w-full max-w-lg md:max-w-xl h-[50vh] top-16 right-0 md:right-44 md:top-14 rounded-lg drop-shadow-2xl flex flex-col">
-			<div className="flex justify-between text-sm border-b border-slate-200 p-3 md:p-5">
-				<div className="font-semibold">{user?.firstName}'s Notifications</div>
-				{unreadCount > 0 && (
-					<span className="bg-maroon text-white text-xs rounded-full p-2">
-						{unreadCount} New
-					</span>
-				)}
-			</div>
-
-			{/* Notifications*/}
-			<div className="flex flex-grow flex-col overflow-y-auto">
-				{notifications?.map((notification, key) => (
-					<div className="relative" onClick={() => handleNotifClick(notification)}>
-						<div
-							className={` absolute z-10 ${
-								notificationToDelete === notification?.notificationId
-									? "visible bg-zinc-50 w-full h-full flex items-center justify-center gap-x-2"
-									: "invisible"
-							} `}
-						>
-							<button
-								className="bg-zinc-200 hover:bg-zinc-300 h-7 w-7 rounded-full flex items-center justify-center"
-								onClick={() => handleCancelDelete()}
-							>
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									fill="none"
-									viewBox="0 0 24 24"
-									strokeWidth={1.5}
-									stroke="currentColor"
-									className="size-4"
-								>
-									<path
-										strokeLinecap="round"
-										strokeLinejoin="round"
-										d="M6 18 18 6M6 6l12 12"
-									/>
-								</svg>
-							</button>
-
-							<button
-								className="bg-zinc-200 hover:bg-zinc-300 h-8 w-fit rounded-full px-4 py-1 text-xs flex items-center justify-center"
-								onClick={() => handleDeleteNotification()}
-							>
-								<div className="pr-1">Delete Notification?</div>
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									fill="none"
-									viewBox="0 0 24 24"
-									strokeWidth={1.5}
-									stroke="currentColor"
-									className="size-3.5"
-								>
-									<path
-										strokeLinecap="round"
-										strokeLinejoin="round"
-										d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
-									/>
-								</svg>
-							</button>
-						</div>
-
-						<div
-							className={`${
-								notification?.read ? "" : null
-							} flex flex-row px-3 py-4 md:px-5 md:py-5 group  
-							${
-								notificationToDelete === notification?.notificationId
-									? ""
-									: "hover:bg-zinc-100 transition duration-300 ease-in-out"
-							}`}
-							key={key}
-						>
-							<div className="w-1/6 md:w-2/12 flex justify-center items-center">
-								<img
-									src={notification?.sender?.image}
-									alt="Avatar"
-									className="rounded-full h-10 w-10 md:h-12 md:w-12"
-								/>
-							</div>
-
-							<div className="w-4/6 md:w-9/12 md:pl-4 flex flex-col justify-center text-xs md:text-sm">
-								{renderNotifText(notification)}
-							</div>
-
+		<div>
+			<div className="bg-white absolute z-10 w-full max-w-lg md:max-w-xl h-[50vh] top-16 right-0 md:right-44 md:top-14 rounded-lg drop-shadow-2xl flex flex-col">
+				<div className="flex justify-between text-sm border-b border-slate-200 p-3 md:p-5">
+					<div className="font-semibold">{user?.firstName}'s Notifications</div>
+					{unreadCount > 0 && (
+						<span className="bg-maroon text-white text-xs rounded-full p-2">
+							{unreadCount} New
+						</span>
+					)}
+				</div>
+				{/* Notifications*/}
+				<div className="flex flex-grow flex-col overflow-y-auto">
+					{notifications?.map((notification, key) => (
+						<div className="relative" onClick={() => handleNotifClick(notification)}>
 							<div
-								className="w-1/6 md:w-1/12 flex items-center justify-end invisible group-hover:visible"
-								onClick={() => handleShowConfirmDelete(notification)}
+								className={` absolute z-10 ${
+									notificationToDelete === notification?.notificationId
+										? "visible bg-zinc-50 w-full h-full flex items-center justify-center gap-x-2"
+										: "invisible"
+								} `}
 							>
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									fill="none"
-									viewBox="0 0 24 24"
-									strokeWidth={1.5}
-									stroke="currentColor"
-									className="w-5 h-5 md:w-6 md:h-6 rounded-full p-1 hover:bg-zinc-200 text-zinc-300 hover:text-zinc-500 transition duration-300 ease-in-out"
+								<button
+									className="bg-zinc-200 hover:bg-zinc-300 h-7 w-7 rounded-full flex items-center justify-center"
+									onClick={() => handleCancelDelete()}
 								>
-									<path
-										strokeLinecap="round"
-										strokeLinejoin="round"
-										d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										fill="none"
+										viewBox="0 0 24 24"
+										strokeWidth={1.5}
+										stroke="currentColor"
+										className="size-4"
+									>
+										<path
+											strokeLinecap="round"
+											strokeLinejoin="round"
+											d="M6 18 18 6M6 6l12 12"
+										/>
+									</svg>
+								</button>
+								<button
+									className="bg-zinc-200 hover:bg-zinc-300 h-8 w-fit rounded-full px-4 py-1 text-xs flex items-center justify-center"
+									onClick={() => handleDeleteNotification()}
+								>
+									<div className="pr-1">Delete Notification?</div>
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										fill="none"
+										viewBox="0 0 24 24"
+										strokeWidth={1.5}
+										stroke="currentColor"
+										className="size-3.5"
+									>
+										<path
+											strokeLinecap="round"
+											strokeLinejoin="round"
+											d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+										/>
+									</svg>
+								</button>
+							</div>
+							<div
+								className={`${
+									notification?.read ? "" : null
+								} flex flex-row px-3 py-4 md:px-5 md:py-5 group
+								${
+									notificationToDelete === notification?.notificationId
+										? ""
+										: "hover:bg-zinc-100 transition duration-300 ease-in-out"
+								}`}
+								key={key}
+							>
+								<div className="w-1/6 md:w-2/12 flex justify-center items-center">
+									<img
+										src={notification?.sender?.image}
+										alt="Avatar"
+										className="rounded-full h-10 w-10 md:h-12 md:w-12"
 									/>
-								</svg>
+								</div>
+								<div className="w-4/6 md:w-9/12 md:pl-4 flex flex-col justify-center text-xs md:text-sm">
+									{renderNotifText(notification)}
+								</div>
+								<div
+									className="w-1/6 md:w-1/12 flex items-center justify-end invisible group-hover:visible"
+									onClick={() => handleShowConfirmDelete(notification)}
+								>
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										fill="none"
+										viewBox="0 0 24 24"
+										strokeWidth={1.5}
+										stroke="currentColor"
+										className="w-5 h-5 md:w-6 md:h-6 rounded-full p-1 hover:bg-zinc-200 text-zinc-300 hover:text-zinc-500 transition duration-300 ease-in-out"
+									>
+										<path
+											strokeLinecap="round"
+											strokeLinejoin="round"
+											d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+										/>
+									</svg>
+								</div>
 							</div>
 						</div>
-					</div>
-				))}
+					))}
+				</div>
 			</div>
+
+			{appointmentModal && (
+				<AppointmentHighlight
+					setShowModal={setAppointmentModal}
+					appointment={appointment}
+				/>
+			)}
+
+			{appointmentDoneModal && (
+				<AppointmentDone setShowModal={setAppointmentDoneModal} appointment={appointment} />
+			)}
 		</div>
 	);
 }
