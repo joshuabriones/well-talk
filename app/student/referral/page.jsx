7ea55f50afe75f1547f95e70fe1de56585fa3b27
keisamae <kheisa.selma@cit.edu>
2024-12-06 10:33:11 +0800

    referral

--- app/student/referral/page.jsx
+++ app/student/referral/page.jsx
@@ -50,6 +50,58 @@ const TableHeaders = ({ handleSort }) => {
   );
 };
 
+const ReferrerTableHeaders = (handleSort) => {
+  return (
+    <thead className="bg-silver px-2 border-b-2 border-maroon rounded-t-2xl">
+      <tr className="font-bold text-center py-4 rounded-t-2xl">
+        <th className="py-4">Referrer</th>
+        <th className="py-4">Relationship</th>
+        <th className="py-4">Status</th>
+        <th className="py-4 hidden lg:table-cell">Reason</th>
+      </tr>
+    </thead>
+  );
+};
+
+const ReferrerTableBody = ({ referrers }) => {
+  return (
+    <tbody>
+      {referrers.map((referrals) => (
+        <tr
+          key={referrals.referralId}
+          onClick={() => handleRowClick(referrals.referralId)}
+          className={`border-slate-100 border-b-2 hover:bg-slate-100 cursor-pointer transition duration-300 ease-in-out`}
+        >
+          <td className="text-center">
+            <div className="text-center">
+              <div>
+                {referrals.referrer.firstName}, {referrals.referrer.lastName}
+              </div>
+            </div>
+          </td>
+          <td className="hidden lg:table-cell ">
+            <div className="text-center">
+              <div className="text-sm">{referrals.relationship}</div>
+            </div>
+          </td>
+          <td className="text-center">
+            <div className="text-center">
+              <div className="text-sm">{referrals.status}</div>
+            </div>
+          </td>
+          <td className="hidden lg:table-cell text-center">
+            <p>
+              {referrals?.reason?.length > 50
+                ? `${referrals?.reason?.substring(0, 40)}...`
+                : referrals?.reason}
+            </p>
+          </td>
+        </tr>
+      ))}
+    </tbody>
+  );
+};
+
 const TableBody = ({ currentList, handleRowClick, showDeleteModal }) => {
   return (
     <tbody>
@@ -177,15 +229,10 @@ const Referral = () => {
 
   // referrals sample
   const [referrals, setReferrals] = useState([{}]);
+  const [referrers, setReferrers] = useState([]);
 
-  /* Handling unauthenticated users */
-  if (Cookies.get("token") === undefined || Cookies.get("token") === null) {
-    return <Load route="login" />;
-  }
+  const [tabs, setTabs] = useState("myReferrals");
 
-  if (userSession && userSession.role !== "student") {
-    return <Load route={userSession.role} />;
-  }
   // sorting
   const [sortOrder, setSortOrder] = useState("asc");
 
@@ -240,6 +287,13 @@ const Referral = () => {
   const indexOfFirstInquiry = indexOfLastInquiry - ReferralsPerPage;
   const currentList = referrals.slice(indexOfFirstInquiry, indexOfLastInquiry);
 
+  const indexOfLastReferrer = currentPage * ReferralsPerPage;
+  const indexOfFirstReferrer = indexOfLastReferrer - ReferralsPerPage;
+  const currentReferrerList = referrers.slice(
+    indexOfFirstReferrer,
+    indexOfLastReferrer
+  );
+
   const handleSort = (column) => {
     const order = sortOrder === "asc" ? "desc" : "asc";
     setSortOrder(order);
@@ -287,12 +341,41 @@ const Referral = () => {
     }
   };
 
+  const fetchWhoReferredMe = async () => {
+    try {
+      const response = await fetch(
+        `${process.env.BASE_URL}${API_ENDPOINT.GET_REFERRERS}${userSession.id}`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${Cookies.get("token")}`,
+          },
+        }
+      );
+      if (!response.ok) {
+        throw new Error("Failed to fetch referrals");
+      }
+      const data = await response.json();
+
+      setReferrers(data);
+    } catch (error) {
+      console.error("Error fetching posts:", error);
+    }
+  };
+
   useEffect(() => {
     if (userSession) {
       fetchReferrals();
+      fetchWhoReferredMe();
     }
   }, []);
 
+  console.log("Referrers: ", referrers);
+
+  const handleTabs = (tab) => {
+    setTabs(tab);
+  };
   return (
     <div className="min-h-screen w-full">
       <Navbar userType="student" />
@@ -300,27 +383,72 @@ const Referral = () => {
         image={hdrReferrals.src}
         desc="Empower your peers! As a student, use this referral portal to recommend friends who may benefit from counseling support. Your understanding and encouragement can help them access the resources they need for personal growth and success."
       />
-
-      <div className="flex flex-col px-4 sm:px-10 md:px-20 lg:px-56 py-10">
-        <TableTitle addReferral={setAddReferral} />
-
-        <div className="overflow-x-auto">
-          <table className="table w-full bg-gray-100">
-            <TableHeaders handleSort={handleSort} />
-            <TableBody
-              currentList={currentList}
-              handleRowClick={handleRowClick}
-              showDeleteModal={showDeleteModal}
-            />
-          </table>
-        </div>
-
-        <PaginationControls
-          currentPage={currentPage}
-          setCurrentPage={setCurrentPage}
-          list={referrals}
-        />
+      <div className="w-full pt-24 flex items-center gap-3 justify-center">
+        <button
+          className={`font-medium px-4 py-2 rounded-full transition-colors duration-200 ${
+            tabs === "myReferrals"
+              ? "bg-maroon text-white"
+              : "border-2 border-maroon text-maroon"
+          }`}
+          onClick={() => handleTabs("myReferrals")}
+        >
+          My Referrals
+        </button>
+        <button
+          className={`font-medium px-4 py-2 rounded-full transition-colors duration-200 ${
+            tabs === "viewReferrer"
+              ? "bg-maroon text-white"
+              : "border-2 border-maroon text-maroon"
+          }`}
+          onClick={() => handleTabs("viewReferrer")}
+        >
+          View Who Referred Me
+        </button>
       </div>
+
+      {tabs === "myReferrals" && (
+        <div className="flex flex-col px-4 sm:px-10 md:px-20 lg:px-56 py-10">
+          <TableTitle addReferral={setAddReferral} />
+
+          <div className="overflow-x-auto">
+            <table className="table w-full bg-gray-100">
+              <TableHeaders handleSort={handleSort} />
+              <TableBody
+                currentList={currentList}
+                handleRowClick={handleRowClick}
+                showDeleteModal={showDeleteModal}
+              />
+            </table>
+          </div>
+
+          <PaginationControls
+            currentPage={currentPage}
+            setCurrentPage={setCurrentPage}
+            list={referrals}
+          />
+        </div>
+      )}
+
+      {tabs === "viewReferrer" && (
+        <div className="flex flex-col px-4 sm:px-10 md:px-20 lg:px-56 py-10">
+          <div className="overflow-x-auto">
+            <table className="table w-full bg-gray-100">
+              <ReferrerTableHeaders handleSort={handleSort} />
+              <ReferrerTableBody
+                referrers={currentReferrerList}
+                handleRowClick={handleRowClick}
+                showDeleteModal={showDeleteModal}
+              />
+            </table>
+          </div>
+
+          <PaginationControls
+            currentPage={currentPage}
+            setCurrentPage={setCurrentPage}
+            list={referrers}
+          />
+        </div>
+      )}
 
       {deleteModal && (
         <ModalDelete
